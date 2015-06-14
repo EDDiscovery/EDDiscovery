49e8e210447e4bea724a7b8ae5b91d298ef1336c
Robert Wahlstrom <robert@alderaan.nu>
2015-06-14 16:52:32 +0200

    Allow regions with , as decimal separator to  also use . as decimal separator

--- EDDiscovery/EDDiscoveryForm.Designer.cs
+++ EDDiscovery/EDDiscoveryForm.Designer.cs
@@ -95,6 +95,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // tabPage2
