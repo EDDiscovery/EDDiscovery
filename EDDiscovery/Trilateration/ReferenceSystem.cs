dcdf50c827c2c9d7120d00d857af7a679bf4a696
Robert Wahlstrom <robert@alderaan.nu>
2015-07-14 14:52:37 +0200

    Start of suggestedsystem calculation

---
+++ EDDiscovery/Trilateration/ReferenceSystem.cs
@@ -0,0 +1,26 @@
+﻿using EDDiscovery;
+using EDDiscovery.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDDiscovery2.Trilateration
+{
+    public class ReferenceSystem : SystemClass
+    {
+        public double Distance;
+        public double Azimuth, Altitude;
+        private SystemClass refSys;
+
+        
+
+        public ReferenceSystem(SystemClass refSys, SystemClass EstimatedPosition)
+        {
+            Azimuth = Math.Atan2(refSys.y - EstimatedPosition.y, refSys.x - EstimatedPosition.x);
+            Distance = SystemData.Distance(refSys, EstimatedPosition);
+            Altitude= Math.Acos(refSys.z-EstimatedPosition.z/Distance);
+        }
+
+    }
+}
---
+++ EDDiscovery/Trilateration/ReferencesSector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDDiscovery2.Trilateration
+{
+    public class ReferencesSector
+    {
+        double WidthAngle;
+        double AzimuthStart;
+        double AltitudeStart;
+
+        public string Name
+        {
+            get
+            {
+                return "Sector:" + (AzimuthStart * 180 / Math.PI).ToString("0") + ":" + (AltitudeStart * 180 / Math.PI).ToString("0");
+            }
+        }
+
+    }
+}
