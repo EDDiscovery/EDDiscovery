f3e2d0da4e64a097ef54c473942a62cebb49dd5c
Robert Wahlstrom <robert@alderaan.nu>
2015-08-11 09:08:32 +0200

    Draw on source image

--- EDDiscovery/2DMap/FormSagCarinaMission.cs
+++ EDDiscovery/2DMap/FormSagCarinaMission.cs
@@ -72,37 +72,50 @@ namespace EDDiscovery2
 
             var history = from systems in _eddiscoveryForm.TravelControl.visitedSystems where systems.time > start && systems.curSystem!=null && systems.curSystem.HasCoordinate == true  orderby systems.time  select systems;
             List<SystemPosition> listHistory = history.ToList<SystemPosition>();
+            Graphics gfx = Graphics.FromImage(panel1.BackgroundImage);
+            Pen pen = new Pen(Color.Red, 2);
 
             if (listHistory.Count > 1)
             {
                 for (int ii = 1; ii < listHistory.Count; ii++)
                 {
-                    DrawLine(listHistory[ii - 1].curSystem, listHistory[ii].curSystem);
+                    DrawLine(gfx, pen, listHistory[ii - 1].curSystem, listHistory[ii].curSystem);
                 }
             }
 
             Point test1  = currentFGEImage.TransformCoordinate(currentFGEImage.BottomLeft);
             Point test2 = currentFGEImage.TransformCoordinate(currentFGEImage.TopRight);
+
+            TestGrid(gfx);
         }
 
-        private void DrawLine(SystemClass sys1, SystemClass sys2)
+        private void DrawLine(Graphics gfx, Pen pen, SystemClass sys1, SystemClass sys2)
         {
-            Graphics gfx = panel1.CreateGraphics();
-
-            gfx.DrawLine(Pens.Red, Transform2Screen(currentFGEImage.TransformCoordinate(new Point((int)sys1.x, (int)sys1.z))), Transform2Screen(currentFGEImage.TransformCoordinate(new Point((int)sys2.x, (int)sys2.z))));
+            gfx.DrawLine(pen, Transform2Screen(currentFGEImage.TransformCoordinate(new Point((int)sys1.x, (int)sys1.z))), Transform2Screen(currentFGEImage.TransformCoordinate(new Point((int)sys2.x, (int)sys2.z))));
         }
+
+
+        private void TestGrid(Graphics gfx)
+        {
+            Pen pointPen = new Pen(Color.LawnGreen, 3);
+
+            for (int x = currentFGEImage.BottomLeft.X; x<= currentFGEImage.BottomRight.X; x+= 1000)
+                for (int z = currentFGEImage.BottomLeft.Y; z<= currentFGEImage.TopLeft.Y; z+= 1000)
+                    gfx.DrawLine(pointPen, currentFGEImage.TransformCoordinate(new Point(x,z)), currentFGEImage.TransformCoordinate(new Point(x+10, z)));
+        }
+
 
         private Point Transform2Screen(Point point)
         {
-            Point np = new Point(point.X / 4, point.Y / 4);
+            //Point np = new Point(point.X / 4, point.Y / 4);
 
-            return np;
+            return point;
         }
 
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            DrawTravelHistory();
+            //DrawTravelHistory();
         }
     }
 }
