01ca988cb7043112a715aea794ce57c5f43d05c6
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2015-10-20 16:29:20 +0200

    EDSM sync

--- EDDiscovery/EDDiscoveryForm.cs
+++ EDDiscovery/EDDiscoveryForm.cs
@@ -35,6 +35,8 @@ namespace EDDiscovery
         readonly string fileTgcDistances;
 
         public event DistancesLoaded OnDistancesLoaded;
+        public EDSMSync edsmsync;
+
 
         public EDDiscoveryForm()
         {
@@ -42,6 +44,8 @@ namespace EDDiscovery
            
             fileTgcSystems = Path.Combine(Tools.GetAppDataDirectory(), "tgcsystems.json");
             fileTgcDistances = Path.Combine(Tools.GetAppDataDirectory(), "tgcdistances.json");
+
+            edsmsync = new EDSMSync(this);
 
             trilaterationControl.InitControl(this);
             travelHistoryControl1.InitControl(this);
@@ -724,6 +728,7 @@ namespace EDDiscovery
         private void EDDiscoveryForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             travelHistoryControl1.netlog.StopMonitor();
+            edsmsync.StopSync();
             SaveSettings();
         }
 
@@ -738,95 +743,7 @@ namespace EDDiscovery
             //            frm.Show();
 
 
-            SQLiteDBClass db = new SQLiteDBClass();
-            EDSMClass edsm = new EDSMClass();
-
-            edsm.apiKey = db.GetSettingString("EDSMApiKey", "");
-            edsm.commanderName = db.GetSettingString("CommanderName", "");
-
-            //string comments =  edsm.GetComments(new DateTime(2015, 1, 1));
-            List<SystemPosition> log;
-            int ret = edsm.GetLogs(new DateTime(2011, 1, 1), out log);
-
-            if (log == null)
-                log = new List<SystemPosition>();
-
-            // Send Unsynced system to EDSM.
-            foreach (var system in travelHistoryControl1.visitedSystems)
-            {
-                string json = null;
-
-                if (system.vs != null && system.vs.EDSM_sync == false)
-                {
-                    // check if it exist in EDSM
-                    SystemPosition ps2 = (from c in log where c.Name == system.Name && c.time.Ticks == system.time.Ticks select c).FirstOrDefault<SystemPosition>();
-                    if (ps2 != null)
-                    {
-                        system.vs.EDSM_sync = true;
-                        system.vs.Update();
-
-                    }
-                    else
-                        json = edsm.SetLog(system.Name, system.time);
-
-                    if (json != null)
-                    {
-                        JObject msg = (JObject)JObject.Parse(json);
-
-                        int msgnum = msg["msgnum"].Value<int>();
-
-                        if (msgnum == 100 || msgnum == 401 || msgnum == 402 || msgnum == 403)
-                        {
-                            if (msgnum == 100)
-                                System.Diagnostics.Trace.WriteLine("New");
-
-                            system.vs.EDSM_sync = true;
-                            system.vs.Update();
-                        }
-                        else
-                        {
-                            System.Diagnostics.Trace.WriteLine("Error sync:" + msgnum.ToString() + " : " + system.Name);
-                        }
-
-
-                    }
-                }
-            }
-
-            TravelLogUnit tlu = null;
-
-            // Check for new systems from EDSM
-            foreach (var system in log)
-            {
-                SystemPosition ps2 = (from c in travelHistoryControl1.visitedSystems where c.Name == system.Name && c.time.Ticks == system.time.Ticks select c).FirstOrDefault<SystemPosition>();
-                if (ps2 == null)  // Add to local DB...
-                {
-                    if (tlu == null) // If we dontt have a travellogunit yet then create it. 
-                    {
-                        tlu = new TravelLogUnit();
-
-                        tlu.type = 2;  // EDSM
-                        tlu.Path = "http://www.edsm.net/api-logs-v1/get-logs";
-                        tlu.Name = "EDSM-" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                        tlu.Size = 0;
-
-                        //tlu.Add();  // Add to Database
-                    }
-
-                    VisitedSystemsClass vs = new VisitedSystemsClass();
-
-                    vs.Source = tlu.id;
-                    vs.Unit = tlu.Name;
-
-                    vs.Name = system.Name;
-                    vs.Time = system.time;
-                    vs.EDSM_sync = true;
-
-                    //vs.Add();  // Add to DB;
-                    System.Diagnostics.Trace.WriteLine("New from EDSM");
-                }
-            }
-
+          
         }
 
         private void addNewStarToolStripMenuItem_Click(object sender, EventArgs e)
