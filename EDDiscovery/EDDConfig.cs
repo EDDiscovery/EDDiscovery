5b1d3501b683bce1fc69d60e56c19b46b296a1c1
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2015-12-15 20:19:16 +0100

    New class EDDConfig

---
+++ EDDiscovery/EDDConfig.cs
@@ -0,0 +1,36 @@
+﻿using EDDiscovery.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDDiscovery2
+{
+    public class EDDConfig
+    {
+        private bool _useDistances;
+        SQLiteDBClass db = new SQLiteDBClass();
+
+        public bool UseDistances
+        {
+            get
+            {
+                return _useDistances;
+            }
+
+            set
+            {
+                _useDistances = value;
+                db.PutSettingBool("EDSMDistances", value);
+
+            }
+        }
+
+        public void Update()
+        {
+            _useDistances = db.GetSettingBool("EDSMDistances", false);
+        }
+
+        
+    }
+}
