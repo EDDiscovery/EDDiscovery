c1a704205aa3197ef79fd089b73beb76bef771f6
Robert Wahlström <finwen_se@hotmail.com>
2015-12-31 02:12:03 +0100

    More work on Material class Added AddMaterialNodeControl

---
+++ EDDiscovery/Prospecting/AddMaterialNodeControl.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace EDDiscovery2.Prospecting
+{
+    partial class AddMaterialNodeControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnInc = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.ColumnDec = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.materialBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.materialBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnName,
+            this.ColumnNr,
+            this.ColumnInc,
+            this.ColumnDec});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(249, 382);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // ColumnName
+            // 
+            this.ColumnName.HeaderText = "Material";
+            this.ColumnName.Name = "ColumnName";
+            this.ColumnName.ReadOnly = true;
+            // 
+            // ColumnNr
+            // 
+            this.ColumnNr.HeaderText = "Nr";
+            this.ColumnNr.MaxInputLength = 10;
+            this.ColumnNr.Name = "ColumnNr";
+            this.ColumnNr.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ColumnNr.Width = 30;
+            // 
+            // ColumnInc
+            // 
+            this.ColumnInc.HeaderText = "Inc";
+            this.ColumnInc.Name = "ColumnInc";
+            this.ColumnInc.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ColumnInc.Text = "+";
+            this.ColumnInc.UseColumnTextForButtonValue = true;
+            this.ColumnInc.Width = 30;
+            // 
+            // ColumnDec
+            // 
+            this.ColumnDec.HeaderText = "Dec";
+            this.ColumnDec.Name = "ColumnDec";
+            this.ColumnDec.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ColumnDec.Text = "-";
+            this.ColumnDec.Width = 30;
+            // 
+            // materialBindingSource
+            // 
+            this.materialBindingSource.DataSource = typeof(EDDiscovery2.Material);
+            // 
+            // AddMaterialNodeControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "AddMaterialNodeControl";
+            this.Size = new System.Drawing.Size(258, 436);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.materialBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNr;
+        private System.Windows.Forms.DataGridViewButtonColumn ColumnInc;
+        private System.Windows.Forms.DataGridViewButtonColumn ColumnDec;
+        private System.Windows.Forms.BindingSource materialBindingSource;
+    }
+}
---
+++ EDDiscovery/Prospecting/AddMaterialNodeControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EDDiscovery2.Prospecting
+{
+    public partial class AddMaterialNodeControl : UserControl
+    {
+        public AddMaterialNodeControl()
+        {
+            InitializeComponent();
+            PopulateRows();
+        }
+
+
+        private void PopulateRows()
+        {
+            List<Material> mlist = Material.GetMaterialList;
+
+            mlist.ForEach(c => c.number = 0);  // Set all material numbers to 0.
+
+            dataGridView1.Rows.Clear();
+
+            foreach (Material mat in mlist)
+            {
+                int rownr;
+                string[] row = new string[] { mat.Name, mat.number.ToString(), "+", "-" };
+
+                dataGridView1.Rows.Add(row);
+                rownr = dataGridView1.Rows.GetLastRow(DataGridViewElementStates.Visible);
+                dataGridView1.Rows[rownr].Tag = mat;
+
+                dataGridView1.Rows[rownr].Cells[0].Style.BackColor = mat.RareityColor;
+
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex==2) // increase
+            {
+
+            }
+            if (e.ColumnIndex == 3) // decreas number.
+            {
+                
+            }
+
+        }
+    }
+}
