8d8930bf7add246231eb1abb8bed15239c4038e4
Robert Wahlström <finwen_se@hotmail.com>
2016-01-01 13:13:14 +0100

    Renamed FormProspecting

---
+++ EDDiscovery/Prospecting/FormProspecting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EDDiscovery2
+{
+    public partial class FormProspecting : Form
+    {
+        public FormProspecting()
+        {
+            InitializeComponent();
+        }
+    }
+}
