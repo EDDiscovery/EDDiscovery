2abd029801e415774acd5a5eeb42affdc38dc331
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-01-02 23:44:26 -0500

    Created keyboard bindings for all the things. Can transform the camera in all directions and rotations

---
+++ EDDiscovery/3DMap/KeyboardActions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDDiscovery2._3DMap
+{
+    public class KeyboardActions
+    {
+        public bool Left { get; set; }
+        public bool Right { get; set; }
+        public bool Up { get; set; }
+        public bool Down { get; set; }
+        public bool Forwards { get; set; }
+        public bool Backwards { get; set; }
+        public bool Pitch { get; set; }
+        public bool Dive { get; set; }
+        public bool YawLeft { get; set; }
+        public bool YawRight { get; set; }
+        public bool RollLeft { get; set;  }
+        public bool RollRight { get; set; }
+        public bool ZoomIn { get; set; }
+        public bool ZoomOut { get; set; }
+
+        public void Reset()
+        {
+            Left = false;
+            Right = false;
+            Up = false;
+            Down = false;
+            Forwards = false;
+            Backwards = false;
+            Pitch = false;
+            Dive = false;
+            YawLeft = false;
+            YawRight = false;
+            RollLeft = false;
+            RollRight = false;
+            ZoomIn = false;
+            ZoomOut = false;
+        }
+
+    }
+}
