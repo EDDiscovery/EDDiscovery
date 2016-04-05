f2a2a062dbe1c01167de54e3ed1d6533cc55b07c
robbyxp1 <robxp@hotmail.com>
2016-04-05 15:53:43 +0100

    Draw the panel icons instead of bitmaps - colours properly
    The panel icons (gripper, close, minimise, ross, eddb) are drawn by
    DrawnPanel instead of bitmaps - allows them to be coloured properly

--- EDDiscovery/Controls/DrawnPanel.cs
+++ EDDiscovery/Controls/DrawnPanel.cs
@@ -9,25 +9,32 @@ namespace ExtendedControls
 {
     class DrawnPanel : Panel
     {
-        public enum ImageType { Close, Minimize, Gripper };
+        public enum ImageType { Close, Minimize, Gripper, EDDB, Ross };
         public ImageType Image { get; set; } = ImageType.Close;
         public int MarginSize { get; set; } = 4;                    // margin around icon
+        
+        private byte limit(float a) { if (a > 255F) return 255; else return (byte)a; }
+        public Color Multiply(Color from, float m) { return Color.FromArgb(from.A, limit((float)from.R * m), limit((float)from.G * m), limit((float)from.B * m)); }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
+            int msize = (MarginSize > 0) ? MarginSize : ClientRectangle.Width / 6;
+            Color pc = (Enabled) ? this.ForeColor : Multiply(this.ForeColor, 0.5F);
+
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            Pen p1 = new Pen(this.ForeColor, 1.0F);
-            Pen p2 = new Pen(this.ForeColor, 2.0F);
+            Pen p1 = new Pen(pc, 1.0F);
+            Pen p2 = new Pen(pc, 2.0F);
 
             int rightpx = ClientRectangle.Width - 1;
             int bottompx = ClientRectangle.Height - 1;
+            int centrehorzpx = (ClientRectangle.Width-1) / 2;
 
-            int leftmarginpx = MarginSize;
-            int rightmarginpx = rightpx - MarginSize;
-            int topmarginpx = MarginSize;
-            int bottommarginpx = bottompx - MarginSize;
+            int leftmarginpx = msize;
+            int rightmarginpx = rightpx - msize;
+            int topmarginpx = msize;
+            int bottommarginpx = bottompx - msize;
 
             if (Image == ImageType.Close)
             {
@@ -42,8 +49,41 @@ namespace ExtendedControls
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    e.Graphics.DrawLine(p1, new Point(rightmarginpx - i * MarginSize, bottompx), new Point(rightpx, bottommarginpx - i * MarginSize));
+                    e.Graphics.DrawLine(p1, new Point(rightmarginpx - i * msize, bottompx), new Point(rightpx, bottommarginpx - i * msize));
                 }
+            }
+            else if (Image == ImageType.EDDB)
+            {
+                Brush bbck = new SolidBrush(pc);
+                Rectangle area = new Rectangle(leftmarginpx, topmarginpx, ClientRectangle.Width - 2 * msize, ClientRectangle.Height - 2 * msize);
+                e.Graphics.FillRectangle(bbck, area);
+                bbck.Dispose();
+
+                Pen pb = new Pen(this.BackColor, 2.0F);
+                Point pt1 = new Point(rightmarginpx, bottommarginpx - msize);
+                Point pt2 = new Point(centrehorzpx-1, bottommarginpx - msize);
+                Point pt3 = new Point(centrehorzpx-1, topmarginpx + msize);
+                Point pt4 = new Point(centrehorzpx-1 - msize, pt3.Y + 2);
+                Point pt5 = new Point(centrehorzpx-1 + msize, pt3.Y + 2);
+
+                e.Graphics.DrawLine(pb, pt1, pt2);
+                e.Graphics.DrawLine(pb, pt2, pt3);
+                e.Graphics.DrawLine(pb, pt2, pt3);
+                e.Graphics.DrawLine(pb, pt4, pt5);
+
+                pb.Dispose();
+            }
+            else if (Image == ImageType.Ross)
+            {
+                Pen pb = new Pen(pc, 3.0F);
+                Point pt1 = new Point(leftmarginpx + 2, bottommarginpx);
+                Point pt2 = new Point(pt1.X, topmarginpx + 4);
+                Point pt3 = new Point(centrehorzpx+2, pt2.Y);
+
+                e.Graphics.DrawLine(pb, pt1, pt2);
+                e.Graphics.DrawLine(pb, pt2, pt3);
+
+                pb.Dispose();
             }
 
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
