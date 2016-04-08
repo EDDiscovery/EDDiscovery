EDDiscovery/EDDiscovery

commit dd786ee248389b23b0fa7285cbf3a886be99b819

Reduce update frequency to reduce CPU and GPU load

EDDiscovery/3DMap/FormMap.cs
@@ -56,7 +56,7 @@ namespace EDDiscovery2
         private float _cameraSlewProgress = 1.0f;
 
         private KeyboardActions _kbdActions = new KeyboardActions();
-        private int _oldTickCount = Environment.TickCount;
+        private long _oldTickCount = DateTime.Now.Ticks / 10000;
         private int _ticks = 0;
 
         private Point _mouseStartRotate;
@@ -79,6 +79,7 @@ namespace EDDiscovery2
 
         private float _znear;
         private float _zfar;
+        private bool _useTimer;
         
         public ISystem CenterSystem {
             get
@@ -526,8 +527,8 @@ namespace EDDiscovery2
         }
         private void CalculateTimeDelta()
         {
-            var tickCount = Environment.TickCount;
-            _ticks = tickCount - _oldTickCount;
+            var tickCount = DateTime.Now.Ticks / 10000;
+            _ticks = (int)(tickCount - _oldTickCount);
             _oldTickCount = tickCount;
         }
 
@@ -843,22 +844,52 @@ namespace EDDiscovery2
             _loaded = true;
 
             Application.Idle += Application_Idle;
-
+            _useTimer = false;
         }
-
 
         private void Application_Idle(object sender, EventArgs e)
         {
-            glControl.Invalidate();
+            if (!_useTimer)
+            {
+                glControl.Invalidate();
+            }
         }
 
         private void glControl_Paint(object sender, PaintEventArgs e)
         {
+            if (!_kbdActions.Any())
+            {
+                _ticks = 0;
+                _oldTickCount = DateTime.Now.Ticks / 10000;
+            }
             CalculateTimeDelta();
             HandleInputs();
             DoCameraSlew();
             UpdateCamera();
             Render();
+
+            if (_kbdActions.Any())
+            {
+                if (_useTimer)
+                {
+                    UpdateTimer.Stop();
+                    _useTimer = false;
+                }
+            }
+            else
+            {
+                if (!_useTimer)
+                {
+                    UpdateTimer.Interval = 100;
+                    UpdateTimer.Start();
+                    _useTimer = true;
+                }
+                else
+                {
+                    UpdateTimer.Stop();
+                    UpdateTimer.Start();
+                }
+            }
         }
 
         private void UpdateCamera()
@@ -1084,6 +1115,39 @@ namespace EDDiscovery2
             {
                 OrientateMapAroundSystem(sys);
             }
+        }
+        
+	private void glControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_useTimer)
+            {
+                glControl.Invalidate();
+            }
+        }
+
+        private void glControl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (_useTimer)
+            {
+                glControl.Invalidate();
+            }
+        }
+
+        private void FormMap_Activated(object sender, EventArgs e)
+        {
+            _useTimer = false;
+            glControl.Invalidate();
+        }
+
+        private void FormMap_Deactivate(object sender, EventArgs e)
+        {
+            _useTimer = true;
+            UpdateTimer.Stop();
+        }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            glControl.Invalidate();
         }
     }
 }

EDDiscovery/3DMap/FormMap.Designer.cs
@@ -56,6 +56,7 @@ namespace EDDiscovery2
             this.labelClickedSystemCoords = new System.Windows.Forms.Label();
             this.dotSelectedSystemCoords = new System.Windows.Forms.PictureBox();
             this.dotSystemCoords = new System.Windows.Forms.PictureBox();
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
             this.toolStripShowAllStars.SuspendLayout();
             this.statusStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dotSelectedSystemCoords)).BeginInit();
@@ -72,10 +73,12 @@ namespace EDDiscovery2
             this.glControl.Name = "glControl";
             this.glControl.Size = new System.Drawing.Size(918, 485);
             this.glControl.TabIndex = 0;
-            this.glControl.VSync = false;
+            this.glControl.VSync = true;
             this.glControl.Load += new System.EventHandler(this.glControl_Load);
             this.glControl.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl_Paint);
             this.glControl.DoubleClick += new System.EventHandler(this.glControl_DoubleClick);
+            this.glControl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl_KeyDown);
+            this.glControl.KeyUp += new System.Windows.Forms.KeyEventHandler(this.glControl_KeyUp);
             this.glControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.glControl_MouseDown);
             this.glControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl_MouseMove);
             this.glControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.glControl_MouseUp);
@@ -275,6 +278,9 @@ namespace EDDiscovery2
             this.dotSystemCoords.Size = new System.Drawing.Size(12, 12);
             this.dotSystemCoords.TabIndex = 25;
             this.dotSystemCoords.TabStop = false;
+            // UpdateTimer
+            // 
+            this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
             // 
             // FormMap
             // 
@@ -296,6 +302,8 @@ namespace EDDiscovery2
             this.Name = "FormMap";
             this.Text = "3D Star Map";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Activated += new System.EventHandler(this.FormMap_Activated);
+            this.Deactivate += new System.EventHandler(this.FormMap_Deactivate);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMap_FormClosing);
             this.Load += new System.EventHandler(this.FormMap_Load);
             this.toolStripShowAllStars.ResumeLayout(false);
@@ -331,5 +339,6 @@ namespace EDDiscovery2
         private Label labelClickedSystemCoords;
         private PictureBox dotSystemCoords;
         private PictureBox dotSelectedSystemCoords;
+        private Timer UpdateTimer;
     }
     }

EDDiscovery/3DMap/FormMap.resx
@@ -491,6 +491,9 @@
   <metadata name="toolTip1.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
     <value>295, 17</value>
   </metadata>
+  <metadata name="UpdateTimer.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
+    <value>392, 17</value>
+  </metadata>
   <data name="$this.Icon" type="System.Drawing.Icon, System.Drawing" mimetype="application/x-microsoft.net.object.bytearray.base64">
     <value>
         AAABAAkAEBAAAAEAIABoBAAAlgAAABgYAAABACAAiAkAAP4EAAAgIAAAAQAgAKgQAACGDgAAMDAAAAEA

commit 9392fdbfd026d541337595b5f5fe415e4df93a72

Fix 3DMap centering
Fixes: dd786ee (Reduce update frequency to reduce CPU and GPU load)

EDDiscovery/3DMap/FormMap.cs
@@ -867,18 +867,18 @@ namespace EDDiscovery2
 
         private void glControl_Paint(object sender, PaintEventArgs e)
         {
-            if (!_kbdActions.Any())
+            CalculateTimeDelta();
+            if (!_kbdActions.Any() && (_cameraSlewProgress == 0.0f || _cameraSlewProgress >= 1.0f))
             {
-                _ticks = 0;
+                _ticks = 1;
                 _oldTickCount = DateTime.Now.Ticks / 10000;
             }
-            CalculateTimeDelta();
             HandleInputs();
             DoCameraSlew();
             UpdateCamera();
             Render();
 
-            if (_kbdActions.Any())
+            if (_kbdActions.Any() || _cameraSlewProgress < 1.0f)
             {
                 if (_useTimer)
                 {