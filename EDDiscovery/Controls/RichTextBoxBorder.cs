5fa63fe6337c5313e9242df8c7ec1a4421205089
robbyxp1 <robxp@hotmail.com>
2016-04-20 13:51:09 +0100

    Added new controls for numeric up/down, fixed travelgrid row header, added two new tooltip colors
    Drawn Panel enabled fixed and changed to showing enabled state instead
    of making the EDDB/Ross buttons non visible
    Numeric up/down control added
    textbox border is now same as other controls
    EDDTheme has two more colors for tooltip
    Imagehandler uses the custom up/down
    Fixed the dgv travel grid row header pain in RowPostPaint to use the
    header cell styles.

--- EDDiscovery/Controls/RichTextBoxBorder.cs
+++ EDDiscovery/Controls/RichTextBoxBorder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,7 @@ namespace ExtendedControls
 
         public RichTextBoxBorder() : base()
         {
+            Debug.Assert(false, "Depreciated - use RichTextBoxScroll");
         }
 
         private const int WM_PAINT = 15;
--- EDDiscovery/Controls/TextBoxBorder.cs
+++ EDDiscovery/Controls/TextBoxBorder.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace ExtendedControls
 {
     public class TextBoxBorder : TextBox
     {
         public Color BorderColor { get; set; } = Color.Transparent;
-        public int BorderPadding { get; set; } = 1;
-        public int BorderSize { get; set; } = 1;
+        public float BorderColorScaling { get; set; } = 0.5F;           // Popup style only
 
         public TextBoxBorder() : base()
         {
@@ -29,30 +29,38 @@ namespace ExtendedControls
                 {
                     Graphics g = Parent.CreateGraphics();
 
-                    Pen p = new Pen(BorderColor);
-                    Pen b = new Pen(BackColor);
-                    int offl = (this.BorderStyle != BorderStyle.None) ? 1 : 1;
-                    int offr = (this.BorderStyle != BorderStyle.None) ? 5 : 1;
+                    Rectangle clientborder = new Rectangle(Location.X - 3, Location.Y - 3, ClientRectangle.Width + 6, ClientRectangle.Height + 6);
 
-                    Rectangle rect = new Rectangle(Location.X - offl, Location.Y - offl, ClientRectangle.Width + offr, ClientRectangle.Height + offr);
+                    Color color1 = BorderColor;
+                    Color color2 = Multiply(BorderColor, BorderColorScaling);
+                    
+                    GraphicsPath g1 = RectCutCorners(clientborder.X + 1, clientborder.Y+1, clientborder.Width - 2, clientborder.Height - 1, 1, 1);
+                    using (Pen pc1 = new Pen(color1, 1.0F))
+                        g.DrawPath(pc1, g1);
 
-                    for (int i = 0; i < BorderPadding; i++)
-                    {
-                        g.DrawRectangle(b, rect);
-                        rect.Inflate(1, 1);
-                    }
-                    for (int i = 0; i < BorderSize; i++)
-                    {
-                        g.DrawRectangle(p, rect);
-                        rect.Inflate(1, 1);
-                    }
+                    GraphicsPath g2 = RectCutCorners(clientborder.X, clientborder.Y, clientborder.Width, clientborder.Height - 1, 2, 2);
+                    using (Pen pc2 = new Pen(color2, 1.0F))
+                        g.DrawPath(pc2, g2);
 
-                    p.Dispose();
-                    b.Dispose();
                     g.Dispose();
                 }
             }
         }
+
+        private GraphicsPath RectCutCorners(int x, int y, int width, int height, int roundnessleft, int roundnessright)
+        {
+            GraphicsPath gr = new GraphicsPath();
+
+            gr.AddLine(x + roundnessleft, y, x + width - 1 - roundnessright, y);
+            gr.AddLine(x + width - 1, y + roundnessright, x + width - 1, y + height - 1 - roundnessright);
+            gr.AddLine(x + width - 1 - roundnessright, y + height - 1, x + roundnessleft, y + height - 1);
+            gr.AddLine(x, y + height - 1 - roundnessleft, x, y + roundnessleft);
+            gr.AddLine(x, y + roundnessleft, x + roundnessleft, y);         // close figure manually, closing it with a break does not seem to work
+            return gr;
+        }
+
+        private byte limit(float a) { if (a > 255F) return 255; else return (byte)a; }
+        public Color Multiply(Color from, float m) { return Color.FromArgb(from.A, limit((float)from.R * m), limit((float)from.G * m), limit((float)from.B * m)); }
     }
 
 }
