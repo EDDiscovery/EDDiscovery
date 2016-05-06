817a1af351b69fb38a0d286a06cb6efd632e44f0
Ben Peddell <klightspeed@killerwolves.net>
2016-05-07 07:10:50 +1000

    Replace DrawnPanel sizing grip with status-strip
    StatusStrip uses ToolStripRenderer.DrawStatusStripSizingGrip
    to draw a sizing grip.  Take advantage of this to draw the
    sizing grip, and use WM_NCHITTEST to inform the system where
    the sizing grip and title bar are in borderless mode.

--- EDDiscovery/EDDiscoveryForm.cs
+++ EDDiscovery/EDDiscoveryForm.cs
@@ -31,9 +31,12 @@ namespace EDDiscovery
         #region Variables
 
         public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CLIENT = 0x1;
         public const int HT_CAPTION = 0x2;
+        public const int HT_BOTTOMRIGHT = 0x11;
         public const int WM_NCL_RESIZE = 0x112;
         public const int HT_RESIZE = 61448;
+        public const int WM_NCHITTEST = 0x84;
 
         private IntPtr SendMessage(int msg, IntPtr wparam, IntPtr lparam)
         {
@@ -119,6 +122,7 @@ namespace EDDiscovery
 
         private void EDDiscoveryForm_Layout(object sender, LayoutEventArgs e)       // Manually position, could not get gripper under tab control with it sizing for the life of me
         {
+            /*
             if (panel_grip.Visible)
             {
                 panel_grip.Location = new Point(this.ClientSize.Width - panel_grip.Size.Width, this.ClientSize.Height - panel_grip.Size.Height);
@@ -126,6 +130,7 @@ namespace EDDiscovery
             }
             else
                 tabControl1.Size = new Size(this.ClientSize.Width, this.ClientSize.Height - tabControl1.Location.Y);
+             */
         }
 
         private void ProcessCommandLineOptions()
@@ -331,8 +336,10 @@ namespace EDDiscovery
             else
                 ToolStripManager.Renderer = new ToolStripProfessionalRenderer();
 
+            this.statusStrip1.Renderer = ToolStripManager.Renderer;
+
             this.FormBorderStyle = theme.WindowsFrame ? FormBorderStyle.Sizable : FormBorderStyle.None;
-            panel_grip.Visible = !theme.WindowsFrame;
+            //panel_grip.Visible = !theme.WindowsFrame;
             panel_close.Visible = !theme.WindowsFrame;
             panel_minimize.Visible = !theme.WindowsFrame;
             label_version.Visible = !theme.WindowsFrame;
@@ -975,6 +982,7 @@ namespace EDDiscovery
             this.WindowState = FormWindowState.Minimized;
         }
 
+        /*
         private void panel_grip_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
@@ -984,6 +992,7 @@ namespace EDDiscovery
                 SendMessage(WM_NCL_RESIZE, (IntPtr)HT_RESIZE, IntPtr.Zero);
             }
         }
+         */
 
         private void changeMapColorToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -1026,6 +1035,34 @@ namespace EDDiscovery
             {
                 this.Capture = false;
                 SendMessage(WM_NCLBUTTONDOWN, (IntPtr)HT_CAPTION, IntPtr.Zero);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_NCHITTEST)
+            {
+                base.WndProc(ref m);
+
+                if ((int)m.Result == HT_CLIENT)
+                {
+                    int x = unchecked((short)((uint)m.LParam & 0xFFFF));
+                    int y = unchecked((short)((uint)m.LParam >> 16));
+                    Point p = PointToClient(new Point(x, y));
+
+                    if (p.X > this.ClientSize.Width - statusStrip1.Height && p.Y > this.ClientSize.Height - statusStrip1.Height)
+                    {
+                        m.Result = (IntPtr)HT_BOTTOMRIGHT;
+                    }
+                    else if (!theme.WindowsFrame)
+                    {
+                        m.Result = (IntPtr)HT_CAPTION;
+                    }
+                }
+            }
+            else
+            {
+                base.WndProc(ref m);
             }
         }
 
