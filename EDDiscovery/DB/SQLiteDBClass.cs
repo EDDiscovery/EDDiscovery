EDDiscovery/EDDiscovery

commit 52cbcc3c700127ea0a432055ebaaec58e97bde38

Only initialize the database once

EDDiscovery/DB/SQLiteDBClass.cs
@@ -77,15 +77,22 @@ namespace EDDiscovery.DB
             {
                 dbfile = GetSQLiteDBFile();
 
-                ConnectionString = "Data Source=" + dbfile + ";Pooling=true;";
-
-                if (!File.Exists(dbfile))
+                if (ConnectionString == null)
                 {
-                    CreateDB(dbfile);
-                }
-                else
-                    UpgradeDB();
+                    ConnectionString = "Data Source=" + dbfile + ";Pooling=true;";
 
+                    if (!File.Exists(dbfile))
+                    {
+                        CreateDB(dbfile);
+
+                        if (!File.Exists(dbfile))
+                        {
+                            CreateDB(dbfile);
+                        }
+                        else
+                            UpgradeDB();
+                    }
+                }
             }
         }
 

commit c17a2723d40242a3b151a485604f7e1d233ae058

Fix an error that snuck in with 52cbcc3
Fix an error where DB upgrade / creation would only occur
if the database file had never been created

Fixes: 52cbcc3 Only initialize the database once

EDDiscovery/DB/SQLiteDBClass.cs
@@ -85,14 +85,9 @@ namespace EDDiscovery.DB
                     if (!File.Exists(dbfile))
                     {
                         CreateDB(dbfile);
-
-                        if (!File.Exists(dbfile))
-                        {
-                            CreateDB(dbfile);
-                        }
-                        else
-                            UpgradeDB();
                     }
+                    else
+                        UpgradeDB();
                 }
             }
         }