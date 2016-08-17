11f4ae8e7365c934d766e1d688024ec74c9c665e
Robert Wahlström <finwen_se@hotmail.com>
2016-08-17 13:34:52 +0200

    Journal classes just a basic start

---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalFileHeader.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    public class JournalFileHeader : JournalEntry
+    {
+        public JournalFileHeader(JObject jo) : base(jo, JournalTypeEnum.FileHeader)
+        {
+        }
+    }
+}
---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalLoadGame.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    public class JournalLoadGame : JournalEntry
+    {
+        public JournalLoadGame(JObject jo) : base(jo, JournalTypeEnum.LoadGame)
+        {
+        }
+
+    }
+}
