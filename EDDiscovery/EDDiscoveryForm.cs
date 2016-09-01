f678cf217549a9527638c398ac1862358c7487d8
Ben Peddell <klightspeed@killerwolves.net>
2016-09-02 02:11:55 +1000

    Remove SystemsUpdating flag
    The SystemsUpdating flag is no longer necessary now that
    the bulk sync is done to a temporary table.

--- EDDiscovery/EDDiscoveryForm.cs
+++ EDDiscovery/EDDiscoveryForm.cs
@@ -80,9 +80,6 @@ namespace EDDiscovery
 
         public CancellationTokenSource CancellationTokenSource { get; private set; } = new CancellationTokenSource();
 
-        public bool SystemsUpdating { get; private set; } = true;
-        public object SystemsUpdatingLock { get; } = new object();
-
         private ManualResetEvent _syncWorkerCompletedEvent = new ManualResetEvent(false);
         private ManualResetEvent _checkSystemsWorkerCompletedEvent = new ManualResetEvent(false);
 
@@ -576,7 +573,7 @@ namespace EDDiscovery
 
             if (!cancelRequested())
             {
-
+                SQLiteDBClass.CreateSystemsTableIndexes();
                 SystemNoteClass.GetAllSystemNotes();                                // fill up memory with notes, bookmarks, galactic mapping
                 BookmarkClass.GetAllBookmarks();
                 galacticMapping.ParseData();                            // at this point, EDSM data is loaded..
@@ -723,11 +720,6 @@ namespace EDDiscovery
 
             try
             {
-                lock (SystemsUpdatingLock)
-                {
-                    SystemsUpdating = true;
-                }
-
                 // Delete all old systems
                 SQLiteDBClass.PutSettingString("EDSMLastSystems", "2010-01-01 00:00:00");
                 SQLiteDBClass.PutSettingString("EDDBSystemsTime", "0");
@@ -936,8 +928,6 @@ namespace EDDiscovery
                     }
                     LogLine("EDSM updated " + updates + " systems.");
                 }
-
-                SystemsUpdating = false;
             }
 
             syncwaseddboredsm = edsmoreddbsync;
