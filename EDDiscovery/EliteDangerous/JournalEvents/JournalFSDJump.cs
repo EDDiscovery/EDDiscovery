a580717298c986c5540c672d0f13037db771660e
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2016-09-07 12:11:37 +0200

    More work on Journal events

--- EDDiscovery/EliteDangerous/JournalEvents/JournalFSDJump.cs
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalFSDJump.cs
@@ -8,8 +8,38 @@ namespace EDDiscovery.EliteDangerous.JournalEvents
 {
     public class JournalFSDJump : JournalLocOrJump
     {
-        public JournalFSDJump(JObject jo) : base(jo, JournalTypeEnum.FSDJump)
+        public JournalFSDJump(JObject evt) : base(evt, JournalTypeEnum.FSDJump)
         {
+            Body = evt.Value<string>("Body");
+            JumpDist = evt.Value<double>("JumpDist");
+            FuelUsed = evt.Value<double>("FuelUsed");
+            FuelLevel = evt.Value<double>("FuelLevel");
+            BoostUsed = evt.Value<bool>("BoostUsed");
+            Faction = evt.Value<string>("Faction");
+            FactionState = evt.Value<string>("FactionState");
+            Allegiance = evt.Value<string>("Allegiance");
+            Economy = evt.Value<string>("Economy");
+            Economy_Localised = evt.Value<string>("Economy_Localised");
+            Government = evt.Value<string>("Government");
+            Government_Localised = evt.Value<string>("Government_Localised");
+            Security = evt.Value<string>("Security");
+            Security_Localised = evt.Value<string>("Security_Localised");
         }
+
+        public string Body { get; set; }
+        public double JumpDist { get; set; }
+        public double FuelUsed { get; set; }
+        public double FuelLevel { get; set; }
+        public bool BoostUsed { get; set; }
+        public string Faction { get; set; }
+        public string FactionState { get; set; }
+        public string Allegiance { get; set; }
+        public string Economy { get; set; }
+        public string Economy_Localised { get; set; }
+        public string Government { get; set; }
+        public string Government_Localised { get; set; }
+        public string Security { get; set; }
+        public string Security_Localised { get; set; }
+
     }
 }
--- EDDiscovery/EliteDangerous/JournalEvents/JournalLoadGame.cs
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalLoadGame.cs
@@ -8,9 +8,26 @@ namespace EDDiscovery.EliteDangerous.JournalEvents
 {
     public class JournalLoadGame : JournalEntry
     {
-        public JournalLoadGame(JObject jo) : base(jo, JournalTypeEnum.LoadGame)
+        public JournalLoadGame(JObject evt) : base(evt, JournalTypeEnum.LoadGame)
         {
+            Commander = evt.Value<string>("Commander");
+            Ship = evt.Value<string>("Ship");
+            ShipId = evt.Value<int>("ShipID");
+            StartLanded = evt.Value<bool>("StartLanded");
+            StartDead = evt.Value<bool>("StartDead");
+            GameMode = evt.Value<string>("GameMode");
+            Group = evt.Value<string>("Group");
+            Credits = evt.Value<int>("Credits");
+            Loan = evt.Value<int>("Loan");
         }
-
+        public string Commander { get; set; }
+        public string Ship { get; set; }
+        public int ShipId { get; set; }
+        public bool StartLanded { get; set; }
+        public bool StartDead { get; set; }
+        public string GameMode { get; set; }
+        public string Group { get; set; }
+        public int Credits { get; set; }
+        public int Loan { get; set; }
     }
 }
