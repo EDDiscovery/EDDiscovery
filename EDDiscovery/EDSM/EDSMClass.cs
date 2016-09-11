51a935dec023ad4858477c805e8328ab7c544705
Ben Peddell <klightspeed@killerwolves.net>
2016-09-11 23:29:03 +1000

    Fail EDSM request on bad server response

--- EDDiscovery/EDSM/EDSMClass.cs
+++ EDDiscovery/EDSM/EDSMClass.cs
@@ -220,12 +220,37 @@ namespace EDDiscovery2.EDSM
 
                 discoveryform.LogLine($"Downloading systems from {lstsystdate.ToLocalTime().ToString()} to {enddate.ToLocalTime().ToString()}");
                 reportProgress(-1, "Requesting systems from EDSM");
-                string json = RequestSystems(lstsystdate, enddate);
+                string json = null;
+
+                try
+                {
+                    json = RequestSystems(lstsystdate, enddate);
+                }
+                catch (WebException ex)
+                {
+                    reportProgress(-1, $"EDSM request failed");
+                    if (ex.Status == WebExceptionStatus.ProtocolError)
+                    {
+                        string status = ((HttpWebResponse)ex.Response).StatusDescription;
+                        discoveryform.LogLine($"Download of EDSM systems from the server failed ({status}), will try next time program is run");
+                    }
+                    else
+                    {
+                        discoveryform.LogLine($"Download of EDSM systems from the server failed ({ex.Status.ToString()}), will try next time program is run");
+                    }
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    reportProgress(-1, $"EDSM request failed");
+                    discoveryform.LogLine($"Download of EDSM systems from the server failed ({ex.Message}), will try next time program is run");
+                    break;
+                }
 
                 if (json == null)
                 {
                     reportProgress(-1, "EDSM request failed");
-                    discoveryform.LogLine("Download of EDSM systems from the server failed, will try next time program is run");
+                    discoveryform.LogLine("Download of EDSM systems from the server failed (no data returned), will try next time program is run");
                     break;
                 }
 
