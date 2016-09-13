9aac53adfe4da8abc3c88bb2d3f42174ad59a311
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2016-09-13 15:53:03 +0200

    Added Supercruise events

---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalProgress.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //When written: at startup
+    //Parameters:
+    //•	Combat: percent progress to next rank
+    //•	Trade: 		“
+    //•	Explore: 	“
+    //•	Empire: 	“
+    //•	Federation: 	“
+    //•	CQC: 		“ ranks: 0=’Helpless’, 1=’Mostly Helpless’, 2=’Amateur’, 3=’Semi Professional’, 4=’Professional’, 5=’Champion’, 6=’Hero’, 7=’Legend’, 8=’Elite’
+
+    public class JournalProgress : JournalEntry
+    {
+
+        public JournalProgress(JObject evt, EDJournalReader reader) : base(evt, JournalTypeEnum.Progress, reader)
+        {
+            Combat = evt.Value<int>("Combat");
+            Trade = evt.Value<int>("Trade");
+            Explore = evt.Value<int>("Explore");
+            Empire = evt.Value<int>("Empire");
+            Federation = evt.Value<int>("Federation");
+            CQC = evt.Value<int>("CQC");
+
+        }
+        public int Combat { get; set; }
+        public int Trade { get; set; }
+        public int Explore { get; set; }
+        public int Empire { get; set; }
+        public int Federation { get; set; }
+        public int CQC { get; set; }
+    }
+}
---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalSupercruiseExit.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //When written: entering supercruise from normal space
+    //Parameters:
+    //•	Starsystem
+    public class JournalSupercruiseExit : JournalEntry
+    {
+        public JournalSupercruiseExit(JObject evt, EDJournalReader reader) : base(evt, JournalTypeEnum.SupercruiseExit, reader)
+        {
+            StarSystem = evt.Value<string>("StarSystem");
+            Body = evt.Value<string>("Body");
+
+        }
+        public string StarSystem { get; set; }
+        public string Body { get; set; }
+
+    }
+}
