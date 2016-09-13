19fb0da99cd8783498d787382e03e994dad8d73a
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2016-09-13 20:05:29 +0200

    comment

--- EDDiscovery/EliteDangerous/JournalEvents/JournalSupercruiseExit.cs
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalSupercruiseExit.cs
@@ -3,9 +3,10 @@ using System.Linq;
 
 namespace EDDiscovery.EliteDangerous.JournalEvents
 {
-    //When written: entering supercruise from normal space
+    //When written: leaving supercruise for normal space
     //Parameters:
     //•	Starsystem
+    //•	Body
     public class JournalSupercruiseExit : JournalEntry
     {
         public JournalSupercruiseExit(JObject evt, EDJournalReader reader) : base(evt, JournalTypeEnum.SupercruiseExit, reader)
