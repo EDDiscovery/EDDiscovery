93cf58e996fc14f1ce1e8b33548bed36ba3a78e0
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2016-09-21 11:03:36 +0200

    Added Shipyard and module events

---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalShipyardSwap.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //When Written: when switching to another ship already stored at this station
+    //Parameters:
+    //•	ShipType: type of ship being switched to
+    //•	ShipID
+    //•	StoreOldShip: (if storing old ship) type of ship being stored
+    //•	StoreShipID
+    //•	SellOldShip: (if selling old ship) type of ship being sold
+    //•	SellShipID  
+    public class JournalShipyardSwap : JournalEntry
+    {
+        public JournalShipyardSwap(JObject evt, EDJournalReader reader) : base(evt, JournalTypeEnum.ShipyardSwap, reader)
+        {
+            ShipType = evt.Value<string>("ShipType");
+            ShipId = evt.Value<int>("ShipID");
+            StoreOldShip = evt.Value<string>("StoreOldShip");
+            StoreShipId = evt.Value<int?>("StoreShipID");
+            SellOldShip = evt.Value<string>("SellOldShip");
+            SellShipId = evt.Value<int?>("SellShipID");
+            SellPrice = evt.Value<int?>("SellPrice");
+        }
+        public string ShipType { get; set; }
+        public int ShipId { get; set; }
+        public string StoreOldShip { get; set; }
+        public int? StoreShipId { get; set; }
+        public string SellOldShip { get; set; }
+        public int? SellShipId { get; set; }
+        public int? SellPrice { get; set; }
+    }
+}
