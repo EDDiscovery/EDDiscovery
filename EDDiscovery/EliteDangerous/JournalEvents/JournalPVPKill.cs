7032d459c8c0e464a0f5965871072c4821e535c8
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2016-09-27 15:40:40 +0200

    Added event PVPKill

---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalPVPKill.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //When written: when this player has killed another player
+    //Parameters: 
+    //•	Victim: name of victim
+    //•	CombatRank: victim’s rank in range 0..8
+    public class JournalPVPKill : JournalEntry
+    {
+        public JournalPVPKill(JObject evt) : base(evt, JournalTypeEnum.PVPKill)
+        {
+            Victim = Tools.GetStringDef(evt["Victim"]);
+
+            CombatRank = (CombatRank)Tools.GetInt(evt["CombatRank"]);
+
+        }
+        public string Victim { get; set; }
+        public CombatRank CombatRank { get; set; }
+
+    }
+}
