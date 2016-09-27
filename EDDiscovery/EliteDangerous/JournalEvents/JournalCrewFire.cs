1753a443b37bda5c7a2ed8b9df74eaf582f2fc2a
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2016-09-27 14:24:55 +0200

    Added journal crew events

---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalCrewFire.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //    When Written: when selling unwanted drones back to the market
+    //Parameters:
+    //•	Type
+    //•	Count
+    //•	SellPrice
+    //•	TotalSale
+
+    public class JournalCrewFire : JournalEntry
+    {
+        public JournalCrewFire(JObject evt) : base(evt, JournalTypeEnum.CrewFire)
+        {
+            Name = Tools.GetStringDef(evt["Name"]);
+
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalCrewHire.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //When Written: when engaging a new member of crew
+    //Parameters:
+    //•	Name
+    //•	Faction
+    //•	Cost
+    //•	CombatRank
+
+    public class JournalCrewHire : JournalEntry
+    {
+        public JournalCrewHire(JObject evt) : base(evt, JournalTypeEnum.CrewHire)
+        {
+            Name = Tools.GetStringDef(evt["Name"]);
+            Faction = Tools.GetStringDef(evt["Faction"]);
+            Cost = Tools.GetInt(evt["Cost"]);
+            CombatRank = (CombatRank)Tools.GetInt(evt["CombatRank"]);
+        }
+        public string Name { get; set; }
+        public string Faction { get; set; }
+        public int Cost { get; set; }
+        public CombatRank CombatRank { get; set; }
+    }
+}
