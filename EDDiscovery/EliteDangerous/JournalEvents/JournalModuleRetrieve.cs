88d4708a1ed6d6a21d5d75b3febf3c19a92ade7d
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2016-09-27 15:30:36 +0200

    Added ModuleStore/Retrieve event

---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalModuleRetrieve.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //When Written: when fetching a previously stored module
+    //Parameters:
+    //•	Slot
+    //•	Ship
+    //•	ShipID
+    //•	RetrievedItem
+    //•	EngineerModifications: name of modification blueprint, if any
+    //•	SwapOutItem (if slot was not empty)
+    //•	Cost
+    public class JournalModuleRetrieve : JournalEntry
+    {
+        public JournalModuleRetrieve(JObject evt) : base(evt, JournalTypeEnum.ModuleRetrieve)
+        {
+            Slot = Tools.GetStringDef(evt["Slot"]);
+            Ship = Tools.GetStringDef(evt["Ship"]);
+            ShipId = Tools.GetInt(evt["ShipId"]);
+            RetrievedItem = Tools.GetStringDef(evt["RetrievedItem"]);
+            EngineerModifications = Tools.GetStringDef(evt["EngineerModifications"]);
+            SwapOutItem = Tools.GetStringDef(evt["SwapOutItem"]);
+            Cost = Tools.GetInt(evt["Cost"]);
+        }
+        public string Slot { get; set; }
+        public string Ship { get; set; }
+        public int ShipId { get; set; }
+        public string RetrievedItem { get; set; }
+        public string EngineerModifications { get; set; }
+        public string SwapOutItem { get; set; }
+        public int Cost { get; set; }
+
+    }
+}
