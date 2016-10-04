15bae67a403ebc5cfa095e5f93f853cd8bd660f4
robbyxp1 <robxp@hotmail.com>
2016-10-04 22:01:26 +0100

    Introduce EDDItemSet journal record, for our material/commodity tracking system

---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalEDDItemSet.cs
@@ -0,0 +1,33 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //When written: by EDD when a user manually sets an item count (material or commodity)
+
+    public class JournalEDDItemSet : JournalEntry
+    {
+        public JournalEDDItemSet(JObject evt) : base(evt, JournalTypeEnum.EDDItemSet)
+        {
+            Materials = evt["Materials"]?.ToObject<MaterialsItems[]>();
+            Commodities = evt["Commodities"]?.ToObject<CommoditiesItems[]>();
+        }
+
+        public MaterialsItems[] Materials { get; set; }
+        public CommoditiesItems[] Commodities { get; set; }
+
+        public class MaterialsItems
+        {
+            public string Name;
+            public int Count;
+        }
+
+        public class CommoditiesItems
+        {
+            public string Name;
+            public int Count;
+            public int Buyprice;
+        }
+    }
+}
