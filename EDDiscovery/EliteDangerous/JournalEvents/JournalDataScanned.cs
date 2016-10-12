92f4856440da32651a03f0e0b8230ea9e14089bf
Robert Wahlström <robert.l.wahlstrom@gmail.com>
2016-10-12 10:08:09 +0200

    Added DataScanned  DatalinkVoucher events

---
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalDataScanned.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EDDiscovery.EliteDangerous.JournalEvents
+{
+    //When written: when scanning a data link
+    //Parameters:
+    //•	Message: message from data link
+    public class JournalDataScanned : JournalEntry
+    {
+        public JournalDataScanned(JObject evt) : base(evt, JournalTypeEnum.DataScanned)
+        {
+            Type = Tools.GetStringDef(evt["Type"]);
+
+        }
+        public string Type { get; set; }
+
+    }
+}
