c7ec53c0cb79a06a0658981afdd512150e345431
Robert Wahlström <finwen_se@hotmail.com>
2016-10-18 01:56:10 +0200

    EDDN upgrades better way to create messages

--- EDDiscovery/EDDN/EDDNClass.cs
+++ EDDiscovery/EDDN/EDDNClass.cs
@@ -43,23 +43,23 @@ namespace EDDiscovery.EDDN
             if (!journal.HasCoordinate)
                 return null;
 
-            JObject msg = new JObject();
+            if (journal.JumpDist <= 0.0)   //  Checkl for old ED 2.1. converted jump.
+                return null;
 
+            JObject msg = new JObject();
+            
             msg["header"] = Header();
             msg["$schemaRef"] = "http://schemas.elite-markets.net/eddn/journal/1/test";
 
-            JObject message = new JObject();
+            JObject message = (JObject) JObject.Parse(journal.EventDataString);
 
-
-            message["StarSystem"] = journal.StarSystem;
-            message["Government"] = journal.Government;
-            message["timestamp"] = journal.EventTimeUTC.ToString("yyyy-MM-ddTHH:mm:ssZ");
-            message["Faction"] = journal.Faction;
-            message["Allegiance"] = journal.Allegiance;
-            message["StarPos"] = new JArray(new float[] { journal.StarPos.X, journal.StarPos.Y, journal.StarPos.Z });
-            message["Security"] = journal.Security;
-            message["event"] = journal.EventTypeStr;
-            message["Economy"] = journal.Economy;
+            message.Remove("Economy_Localised");
+            message.Remove("Government_Localised");
+            message.Remove("Security_Localised");
+            message.Remove("JumpDist");
+            message.Remove("FuelUsed");
+            message.Remove("FuelLevel");
+            message.Remove("EDDMapColor");
 
             msg["message"] = message;
             return msg;
@@ -72,24 +72,17 @@ namespace EDDiscovery.EDDN
             msg["header"] = Header();
             msg["$schemaRef"] = "http://schemas.elite-markets.net/eddn/journal/1/test";
 
-            JObject message = new JObject();
+            JObject message = (JObject)JObject.Parse(journal.EventDataString);
 
-            message["StarSystem"] = journal.StarSystem;
-            message["StationName"] = journal.StationName;
-            message["StationType"] = journal.StationType;
-            message["Faction"] = journal.Faction;
-            message["Government"] = journal.Government;
-            message["timestamp"] = journal.EventTimeUTC.ToString("yyyy-MM-ddTHH:mm:ssZ");
-            message["Allegiance"] = journal.Allegiance;
-            message["Security"] = journal.Security;
-            message["event"] = journal.EventTypeStr;
-            message["Economy"] = journal.Economy;
+            message.Remove("Economy_Localised");
+            message.Remove("Government_Localised");
+            message.Remove("Security_Localised");
 
             msg["message"] = message;
             return msg;
         }
 
-        public JObject CreateEDDNMessage(JournalScan journal, string starSystem)
+        public JObject CreateEDDNMessage(JournalScan journal, string starSystem, double x, double y, double z)
         {
             JObject msg = new JObject();
 
@@ -98,55 +91,10 @@ namespace EDDiscovery.EDDN
             msg["header"] = Header();
             msg["$schemaRef"] = "http://schemas.elite-markets.net/eddn/journal/1/test";
 
-            JObject message = new JObject();
+            JObject message = (JObject)JObject.Parse(journal.EventDataString);
 
-            message["timestamp"] = journal.EventTimeUTC.ToString("yyyy-MM-ddTHH:mm:ssZ");
-            message["event"] = journal.EventTypeStr;
             message["StarSystem"] = starSystem;
-
-            if (journal.StarType != null && !journal.StarType.Equals("")) // check if star.
-            {
-                message["Bodyname"] = journal.BodyName;
-                message["DistanceFromArrivalLS"] = journal.DistanceFromArrivalLS;
-                message["StarType"] = journal.StarType;
-                message["StellarMass"] = journal.StellarMass;
-                message["Radius"] = journal.Radius;
-                message["AbsoluteMagnitude"] = journal.AbsoluteMagnitude;
-                message["OrbitalPeriod"] = journal.OrbitalPeriod;
-                message["RotationPeriod"] = journal.RotationPeriod;
-
-                if (journal.Rings!= null && journal.Rings.Length > 0)
-                {
-                    message["Rings"] = JArray.FromObject(journal.Rings);
-                }
-            }
-            else
-            {
-                message["Bodyname"] = journal.BodyName;
-                message["DistanceFromArrivalLS"] = journal.DistanceFromArrivalLS;
-                message["TidalLock"] = journal.TidalLock;
-                message["TerraformState"] = journal.TerraformState;
-                message["PlanetClass"] = journal.PlanetClass;
-                message["Atmosphere"] = journal.Atmosphere;
-                message["Volcanism"] = journal.Volcanism;
-                message["SurfaceGravity"] = journal.SurfaceGravity;
-                message["SurfaceTemperature"] = journal.SurfaceTemperature;
-                message["SurfacePressure"] = journal.SurfacePressure;
-                message["Landable"] = journal.Landable;
-                message["OrbitalPeriod"] = journal.OrbitalPeriod;
-                message["RotationPeriod"] = journal.RotationPeriod;
-
-                if (journal.Rings != null && journal.Rings.Length > 0)
-                {
-                    message["Rings"] = JArray.FromObject(journal.Rings);
-                }
-
-                if (journal.Materials.Count > 0)
-                {
-                    message["Materials"] = JObject.Parse(journal.MaterialsString);
-                }
-            }
-
+            message["StarPos"] = new JArray(new float[] { (float)x, (float)y, (float)z });
 
             msg["message"] = message;
             return msg;
