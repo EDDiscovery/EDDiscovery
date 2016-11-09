a806645376c0d9332ce6be3ebadafb7a971f9db8
robbyxp1 <robxp@hotmail.com>
2016-11-09 00:45:59 +0000

    Fix to mat/commodities buy/sell..

--- EDDiscovery/EliteDangerous/JournalEvents/JournalMarketSell.cs
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalMarketSell.cs
@@ -47,7 +47,7 @@ namespace EDDiscovery.EliteDangerous.JournalEvents
         public void Ledger(EDDiscovery2.DB.MaterialCommoditiesLedger mcl, DB.SQLiteConnectionUser conn)
         {
             EDDiscovery2.DB.MaterialCommodities mc = mcl.GetMaterialCommodity(EDDiscovery2.DB.MaterialCommodities.CommodityCategory, Type, conn);
-            mcl.AddEvent(Id, EventTimeUTC, EventTypeID, mc.name + " " + mc.count.ToString(), SellPrice, (double)(SellPrice - AvgPricePaid) / (double)Count);
+            mcl.AddEvent(Id, EventTimeUTC, EventTypeID, mc.name + " " + Count + " Avg " + AvgPricePaid, SellPrice, (double)(SellPrice - AvgPricePaid));
         }
 
     }
