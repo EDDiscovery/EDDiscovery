f648328c7a36d50f3449abc2624b212a5303a7b4
robbyxp1 <robxp@hotmail.com>
2016-11-24 23:51:07 +0000

    Added a ontop button to system menu/form on pop up panels.  Scroll bar on scan disappears when not needed

--- EDDiscovery/Forms/UserControlForm.Designer.cs
+++ EDDiscovery/Forms/UserControlForm.Designer.cs
@@ -29,10 +29,11 @@
         private void InitializeComponent()
         {
             this.label_index = new System.Windows.Forms.Label();
-            this.panel_minimize = new ExtendedControls.DrawnPanel();
-            this.panel_close = new ExtendedControls.DrawnPanel();
-            this.statusStripCustom1 = new ExtendedControls.StatusStripCustom();
             this.labelControlText = new System.Windows.Forms.Label();
+            this.statusStripCustom1 = new ExtendedControls.StatusStripCustom();
+            this.panel_close = new ExtendedControls.DrawnPanel();
+            this.panel_ontop = new ExtendedControls.DrawnPanel();
+            this.panel_minimize = new ExtendedControls.DrawnPanel();
             this.SuspendLayout();
             // 
             // label_index
@@ -44,22 +45,22 @@
             this.label_index.TabIndex = 23;
             this.label_index.Text = "N/A";
             // 
-            // panel_minimize
+            // labelControlText
             // 
-            this.panel_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel_minimize.BackColor = System.Drawing.SystemColors.Control;
-            this.panel_minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel_minimize.DrawnImage = null;
-            this.panel_minimize.ImageSelected = ExtendedControls.DrawnPanel.ImageType.Minimize;
-            this.panel_minimize.ImageText = null;
-            this.panel_minimize.Location = new System.Drawing.Point(580, -2);
-            this.panel_minimize.MarginSize = 6;
-            this.panel_minimize.MouseOverColor = System.Drawing.Color.White;
-            this.panel_minimize.MouseSelectedColor = System.Drawing.Color.Green;
-            this.panel_minimize.Name = "panel_minimize";
-            this.panel_minimize.Size = new System.Drawing.Size(24, 24);
-            this.panel_minimize.TabIndex = 24;
-            this.panel_minimize.Click += new System.EventHandler(this.panel_minimize_Click);
+            this.labelControlText.AutoSize = true;
+            this.labelControlText.Location = new System.Drawing.Point(110, 2);
+            this.labelControlText.Name = "labelControlText";
+            this.labelControlText.Size = new System.Drawing.Size(27, 13);
+            this.labelControlText.TabIndex = 23;
+            this.labelControlText.Text = "N/A";
+            // 
+            // statusStripCustom1
+            // 
+            this.statusStripCustom1.Location = new System.Drawing.Point(0, 536);
+            this.statusStripCustom1.Name = "statusStripCustom1";
+            this.statusStripCustom1.Size = new System.Drawing.Size(634, 22);
+            this.statusStripCustom1.TabIndex = 26;
+            this.statusStripCustom1.Text = "statusStripCustom1";
             // 
             // panel_close
             // 
@@ -78,22 +79,39 @@
             this.panel_close.TabIndex = 25;
             this.panel_close.Click += new System.EventHandler(this.panel_close_Click);
             // 
-            // statusStripCustom1
+            // panel_ontop
             // 
-            this.statusStripCustom1.Location = new System.Drawing.Point(0, 536);
-            this.statusStripCustom1.Name = "statusStripCustom1";
-            this.statusStripCustom1.Size = new System.Drawing.Size(634, 22);
-            this.statusStripCustom1.TabIndex = 26;
-            this.statusStripCustom1.Text = "statusStripCustom1";
+            this.panel_ontop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_ontop.BackColor = System.Drawing.SystemColors.Control;
+            this.panel_ontop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel_ontop.DrawnImage = null;
+            this.panel_ontop.ImageSelected = ExtendedControls.DrawnPanel.ImageType.Floating;
+            this.panel_ontop.ImageText = null;
+            this.panel_ontop.Location = new System.Drawing.Point(550, -2);
+            this.panel_ontop.MarginSize = 6;
+            this.panel_ontop.MouseOverColor = System.Drawing.Color.White;
+            this.panel_ontop.MouseSelectedColor = System.Drawing.Color.Green;
+            this.panel_ontop.Name = "panel_ontop";
+            this.panel_ontop.Size = new System.Drawing.Size(24, 24);
+            this.panel_ontop.TabIndex = 24;
+            this.panel_ontop.Click += new System.EventHandler(this.panel_ontop_Click);
             // 
-            // labelControlText
+            // panel_minimize
             // 
-            this.labelControlText.AutoSize = true;
-            this.labelControlText.Location = new System.Drawing.Point(110, 2);
-            this.labelControlText.Name = "labelControlText";
-            this.labelControlText.Size = new System.Drawing.Size(27, 13);
-            this.labelControlText.TabIndex = 23;
-            this.labelControlText.Text = "N/A";
+            this.panel_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_minimize.BackColor = System.Drawing.SystemColors.Control;
+            this.panel_minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel_minimize.DrawnImage = null;
+            this.panel_minimize.ImageSelected = ExtendedControls.DrawnPanel.ImageType.Minimize;
+            this.panel_minimize.ImageText = null;
+            this.panel_minimize.Location = new System.Drawing.Point(580, -2);
+            this.panel_minimize.MarginSize = 6;
+            this.panel_minimize.MouseOverColor = System.Drawing.Color.White;
+            this.panel_minimize.MouseSelectedColor = System.Drawing.Color.Green;
+            this.panel_minimize.Name = "panel_minimize";
+            this.panel_minimize.Size = new System.Drawing.Size(24, 24);
+            this.panel_minimize.TabIndex = 24;
+            this.panel_minimize.Click += new System.EventHandler(this.panel_minimize_Click);
             // 
             // UserControlForm
             // 
@@ -102,6 +120,7 @@
             this.ClientSize = new System.Drawing.Size(634, 558);
             this.Controls.Add(this.statusStripCustom1);
             this.Controls.Add(this.panel_close);
+            this.Controls.Add(this.panel_ontop);
             this.Controls.Add(this.panel_minimize);
             this.Controls.Add(this.labelControlText);
             this.Controls.Add(this.label_index);
@@ -123,5 +142,6 @@
         private ExtendedControls.DrawnPanel panel_close;
         private ExtendedControls.StatusStripCustom statusStripCustom1;
         private System.Windows.Forms.Label labelControlText;
+        private ExtendedControls.DrawnPanel panel_ontop;
     }
 }
