e5eebcfd4b4188643a6117233bff9cafc8f32d65
robbyxp1 <robxp@hotmail.com>
2016-12-19 16:53:33 +0000

    Trippanel skeleton put in

---
+++ EDDiscovery/UserControls/UserControlTrippanel.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace EDDiscovery.UserControls
+{
+    partial class UserControlTrippanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new ExtendedControls.PictureBoxHotspot();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(728, 570);
+            this.pictureBox.TabIndex = 0;
+            // 
+            // UserControlTrippanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox);
+            this.Name = "UserControlTrippanel";
+            this.Size = new System.Drawing.Size(728, 570);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ExtendedControls.PictureBoxHotspot pictureBox;
+    }
+}
