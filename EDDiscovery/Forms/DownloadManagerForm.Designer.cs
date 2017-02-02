0f0d555d42951695b5c1ebc59e868173281daabb
robbyxp1 <robxp@hotmail.com>
2017-02-02 13:34:08 +0000

    Made combo box go up if no space down.. Downloadform joins thread, works in themes, downloads github

--- EDDiscovery/Forms/DownloadManagerForm.Designer.cs
+++ EDDiscovery/Forms/DownloadManagerForm.Designer.cs
@@ -28,21 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DownloadManagerForm));
             this.panelTop = new System.Windows.Forms.Panel();
+            this.panel_close = new ExtendedControls.DrawnPanel();
+            this.panel_minimize = new ExtendedControls.DrawnPanel();
             this.label_index = new System.Windows.Forms.Label();
             this.panelOuter = new System.Windows.Forms.Panel();
-            this.panelOK = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.panelVScroll = new ExtendedControls.PanelVScroll();
             this.vScrollBarCustom1 = new ExtendedControls.VScrollBarCustom();
             this.richTextBoxScrollDescription = new ExtendedControls.RichTextBoxScroll();
-            this.panel_close = new ExtendedControls.DrawnPanel();
-            this.panel_minimize = new ExtendedControls.DrawnPanel();
+            this.panelOK = new System.Windows.Forms.Panel();
             this.buttonOK = new ExtendedControls.ButtonExt();
             this.statusStripCustom = new ExtendedControls.StatusStripCustom();
             this.panelTop.SuspendLayout();
             this.panelOuter.SuspendLayout();
-            this.panelOK.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.panelVScroll.SuspendLayout();
+            this.panelOK.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelTop
@@ -53,93 +59,9 @@
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTop.Location = new System.Drawing.Point(0, 0);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(1205, 24);
+            this.panelTop.Size = new System.Drawing.Size(1043, 32);
             this.panelTop.TabIndex = 30;
-            // 
-            // label_index
-            // 
-            this.label_index.AutoSize = true;
-            this.label_index.Location = new System.Drawing.Point(3, 8);
-            this.label_index.Name = "label_index";
-            this.label_index.Size = new System.Drawing.Size(27, 13);
-            this.label_index.TabIndex = 23;
-            this.label_index.Text = "N/A";
-            this.label_index.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label_index_MouseDown);
-            // 
-            // panelOuter
-            // 
-            this.panelOuter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelOuter.Controls.Add(this.panelVScroll);
-            this.panelOuter.Controls.Add(this.richTextBoxScrollDescription);
-            this.panelOuter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelOuter.Location = new System.Drawing.Point(0, 24);
-            this.panelOuter.Name = "panelOuter";
-            this.panelOuter.Padding = new System.Windows.Forms.Padding(3);
-            this.panelOuter.Size = new System.Drawing.Size(1205, 643);
-            this.panelOuter.TabIndex = 32;
-            // 
-            // panelOK
-            // 
-            this.panelOK.Controls.Add(this.buttonOK);
-            this.panelOK.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelOK.Location = new System.Drawing.Point(0, 667);
-            this.panelOK.Name = "panelOK";
-            this.panelOK.Size = new System.Drawing.Size(1205, 30);
-            this.panelOK.TabIndex = 10;
-            // 
-            // panelVScroll
-            // 
-            this.panelVScroll.Controls.Add(this.vScrollBarCustom1);
-            this.panelVScroll.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelVScroll.InternalMargin = new System.Windows.Forms.Padding(0);
-            this.panelVScroll.Location = new System.Drawing.Point(3, 3);
-            this.panelVScroll.Name = "panelVScroll";
-            this.panelVScroll.ScrollBarWidth = 20;
-            this.panelVScroll.Size = new System.Drawing.Size(1197, 478);
-            this.panelVScroll.TabIndex = 2;
-            this.panelVScroll.VerticalScrollBarDockRight = true;
-            // 
-            // vScrollBarCustom1
-            // 
-            this.vScrollBarCustom1.ArrowBorderColor = System.Drawing.Color.LightBlue;
-            this.vScrollBarCustom1.ArrowButtonColor = System.Drawing.Color.LightGray;
-            this.vScrollBarCustom1.ArrowColorScaling = 0.5F;
-            this.vScrollBarCustom1.ArrowDownDrawAngle = 270F;
-            this.vScrollBarCustom1.ArrowUpDrawAngle = 90F;
-            this.vScrollBarCustom1.BorderColor = System.Drawing.Color.White;
-            this.vScrollBarCustom1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.vScrollBarCustom1.HideScrollBar = false;
-            this.vScrollBarCustom1.LargeChange = 10;
-            this.vScrollBarCustom1.Location = new System.Drawing.Point(1177, 0);
-            this.vScrollBarCustom1.Maximum = -468;
-            this.vScrollBarCustom1.Minimum = 0;
-            this.vScrollBarCustom1.MouseOverButtonColor = System.Drawing.Color.Green;
-            this.vScrollBarCustom1.MousePressedButtonColor = System.Drawing.Color.Red;
-            this.vScrollBarCustom1.Name = "vScrollBarCustom1";
-            this.vScrollBarCustom1.Size = new System.Drawing.Size(20, 478);
-            this.vScrollBarCustom1.SliderColor = System.Drawing.Color.DarkGray;
-            this.vScrollBarCustom1.SmallChange = 1;
-            this.vScrollBarCustom1.TabIndex = 0;
-            this.vScrollBarCustom1.Text = "vScrollBarCustom1";
-            this.vScrollBarCustom1.ThumbBorderColor = System.Drawing.Color.Yellow;
-            this.vScrollBarCustom1.ThumbButtonColor = System.Drawing.Color.DarkBlue;
-            this.vScrollBarCustom1.ThumbColorScaling = 0.5F;
-            this.vScrollBarCustom1.ThumbDrawAngle = 0F;
-            this.vScrollBarCustom1.Value = -468;
-            this.vScrollBarCustom1.ValueLimited = -468;
-            // 
-            // richTextBoxScrollDescription
-            // 
-            this.richTextBoxScrollDescription.BorderColor = System.Drawing.Color.Transparent;
-            this.richTextBoxScrollDescription.BorderColorScaling = 0.5F;
-            this.richTextBoxScrollDescription.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.richTextBoxScrollDescription.HideScrollBar = true;
-            this.richTextBoxScrollDescription.Location = new System.Drawing.Point(3, 481);
-            this.richTextBoxScrollDescription.Name = "richTextBoxScrollDescription";
-            this.richTextBoxScrollDescription.ScrollBarWidth = 20;
-            this.richTextBoxScrollDescription.ShowLineCount = false;
-            this.richTextBoxScrollDescription.Size = new System.Drawing.Size(1197, 157);
-            this.richTextBoxScrollDescription.TabIndex = 1;
+            this.panelTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label_index_MouseDown);
             // 
             // panel_close
             // 
@@ -149,7 +71,7 @@
             this.panel_close.DrawnImage = null;
             this.panel_close.ImageSelected = ExtendedControls.DrawnPanel.ImageType.Close;
             this.panel_close.ImageText = null;
-            this.panel_close.Location = new System.Drawing.Point(1182, 0);
+            this.panel_close.Location = new System.Drawing.Point(1020, 0);
             this.panel_close.MarginSize = 6;
             this.panel_close.MouseOverColor = System.Drawing.Color.White;
             this.panel_close.MouseSelectedColor = System.Drawing.Color.Green;
@@ -167,7 +89,7 @@
             this.panel_minimize.DrawnImage = null;
             this.panel_minimize.ImageSelected = ExtendedControls.DrawnPanel.ImageType.Minimize;
             this.panel_minimize.ImageText = null;
-            this.panel_minimize.Location = new System.Drawing.Point(1152, 0);
+            this.panel_minimize.Location = new System.Drawing.Point(990, 0);
             this.panel_minimize.MarginSize = 6;
             this.panel_minimize.MouseOverColor = System.Drawing.Color.White;
             this.panel_minimize.MouseSelectedColor = System.Drawing.Color.Green;
@@ -177,13 +99,115 @@
             this.panel_minimize.TabIndex = 26;
             this.panel_minimize.Click += new System.EventHandler(this.panel_minimize_Click);
             // 
+            // label_index
+            // 
+            this.label_index.AutoSize = true;
+            this.label_index.Location = new System.Drawing.Point(3, 8);
+            this.label_index.Name = "label_index";
+            this.label_index.Size = new System.Drawing.Size(88, 13);
+            this.label_index.TabIndex = 23;
+            this.label_index.Text = "Add-On Manager";
+            this.label_index.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label_index_MouseDown);
+            // 
+            // panelOuter
+            // 
+            this.panelOuter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelOuter.Controls.Add(this.splitContainer1);
+            this.panelOuter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelOuter.Location = new System.Drawing.Point(0, 32);
+            this.panelOuter.Name = "panelOuter";
+            this.panelOuter.Padding = new System.Windows.Forms.Padding(3);
+            this.panelOuter.Size = new System.Drawing.Size(1043, 575);
+            this.panelOuter.TabIndex = 32;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panelVScroll);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBoxScrollDescription);
+            this.splitContainer1.Size = new System.Drawing.Size(1035, 567);
+            this.splitContainer1.SplitterDistance = 355;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // panelVScroll
+            // 
+            this.panelVScroll.Controls.Add(this.vScrollBarCustom1);
+            this.panelVScroll.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelVScroll.InternalMargin = new System.Windows.Forms.Padding(0);
+            this.panelVScroll.Location = new System.Drawing.Point(0, 0);
+            this.panelVScroll.Name = "panelVScroll";
+            this.panelVScroll.ScrollBarWidth = 20;
+            this.panelVScroll.Size = new System.Drawing.Size(1035, 355);
+            this.panelVScroll.TabIndex = 2;
+            this.panelVScroll.VerticalScrollBarDockRight = true;
+            // 
+            // vScrollBarCustom1
+            // 
+            this.vScrollBarCustom1.ArrowBorderColor = System.Drawing.Color.LightBlue;
+            this.vScrollBarCustom1.ArrowButtonColor = System.Drawing.Color.LightGray;
+            this.vScrollBarCustom1.ArrowColorScaling = 0.5F;
+            this.vScrollBarCustom1.ArrowDownDrawAngle = 270F;
+            this.vScrollBarCustom1.ArrowUpDrawAngle = 90F;
+            this.vScrollBarCustom1.BorderColor = System.Drawing.Color.White;
+            this.vScrollBarCustom1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.vScrollBarCustom1.HideScrollBar = false;
+            this.vScrollBarCustom1.LargeChange = 10;
+            this.vScrollBarCustom1.Location = new System.Drawing.Point(1015, 0);
+            this.vScrollBarCustom1.Maximum = -345;
+            this.vScrollBarCustom1.Minimum = 0;
+            this.vScrollBarCustom1.MouseOverButtonColor = System.Drawing.Color.Green;
+            this.vScrollBarCustom1.MousePressedButtonColor = System.Drawing.Color.Red;
+            this.vScrollBarCustom1.Name = "vScrollBarCustom1";
+            this.vScrollBarCustom1.Size = new System.Drawing.Size(20, 355);
+            this.vScrollBarCustom1.SliderColor = System.Drawing.Color.DarkGray;
+            this.vScrollBarCustom1.SmallChange = 1;
+            this.vScrollBarCustom1.TabIndex = 0;
+            this.vScrollBarCustom1.Text = "vScrollBarCustom1";
+            this.vScrollBarCustom1.ThumbBorderColor = System.Drawing.Color.Yellow;
+            this.vScrollBarCustom1.ThumbButtonColor = System.Drawing.Color.DarkBlue;
+            this.vScrollBarCustom1.ThumbColorScaling = 0.5F;
+            this.vScrollBarCustom1.ThumbDrawAngle = 0F;
+            this.vScrollBarCustom1.Value = -345;
+            this.vScrollBarCustom1.ValueLimited = -345;
+            // 
+            // richTextBoxScrollDescription
+            // 
+            this.richTextBoxScrollDescription.BorderColor = System.Drawing.Color.Transparent;
+            this.richTextBoxScrollDescription.BorderColorScaling = 0.5F;
+            this.richTextBoxScrollDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxScrollDescription.HideScrollBar = true;
+            this.richTextBoxScrollDescription.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxScrollDescription.Name = "richTextBoxScrollDescription";
+            this.richTextBoxScrollDescription.ScrollBarWidth = 20;
+            this.richTextBoxScrollDescription.ShowLineCount = false;
+            this.richTextBoxScrollDescription.Size = new System.Drawing.Size(1035, 208);
+            this.richTextBoxScrollDescription.TabIndex = 1;
+            // 
+            // panelOK
+            // 
+            this.panelOK.Controls.Add(this.buttonOK);
+            this.panelOK.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelOK.Location = new System.Drawing.Point(0, 607);
+            this.panelOK.Name = "panelOK";
+            this.panelOK.Size = new System.Drawing.Size(1043, 30);
+            this.panelOK.TabIndex = 10;
+            // 
             // buttonOK
             // 
             this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOK.BorderColorScaling = 1.25F;
             this.buttonOK.ButtonColorScaling = 0.5F;
             this.buttonOK.ButtonDisabledScaling = 0.5F;
-            this.buttonOK.Location = new System.Drawing.Point(1126, 4);
+            this.buttonOK.Location = new System.Drawing.Point(964, 4);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
             this.buttonOK.TabIndex = 7;
@@ -193,9 +217,9 @@
             // 
             // statusStripCustom
             // 
-            this.statusStripCustom.Location = new System.Drawing.Point(0, 697);
+            this.statusStripCustom.Location = new System.Drawing.Point(0, 637);
             this.statusStripCustom.Name = "statusStripCustom";
-            this.statusStripCustom.Size = new System.Drawing.Size(1205, 22);
+            this.statusStripCustom.Size = new System.Drawing.Size(1043, 22);
             this.statusStripCustom.TabIndex = 31;
             this.statusStripCustom.Text = "statusStripCustom1";
             // 
@@ -203,19 +227,25 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1205, 719);
+            this.ClientSize = new System.Drawing.Size(1043, 659);
             this.Controls.Add(this.panelOuter);
             this.Controls.Add(this.panelTop);
             this.Controls.Add(this.panelOK);
             this.Controls.Add(this.statusStripCustom);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DownloadManagerForm";
-            this.Text = "DownloadManager";
+            this.Text = "Add-On Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DownloadManagerForm_FormClosing);
             this.Shown += new System.EventHandler(this.DownloadManager_Shown);
             this.panelTop.ResumeLayout(false);
             this.panelTop.PerformLayout();
             this.panelOuter.ResumeLayout(false);
-            this.panelOK.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.panelVScroll.ResumeLayout(false);
+            this.panelOK.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -234,5 +264,6 @@
         private ExtendedControls.RichTextBoxScroll richTextBoxScrollDescription;
         private ExtendedControls.PanelVScroll panelVScroll;
         private ExtendedControls.VScrollBarCustom vScrollBarCustom1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
