b6625f576ea64f0fbb455a5ffc52edd97ab20cb8
phroggie <phroggster@gmail.com>
2017-02-18 01:55:33 -0600

    WM_MOVE all EM, HT, MF, and WM constants to Win32Constants
    This greatly cleans up a number of classes that all had this information
    	duplicated, and it now even comes with documentation.
    
    Numerous (IntPtr)0 -> IntPtr.Zero changes throughout.
    
    UserControlForm:
    	Don't bother using AppendMenu if GetSystemMenu failed.
    	Make SYSMENU_ items const.

--- EDDiscovery/EDDiscoveryForm.cs
+++ EDDiscovery/EDDiscoveryForm.cs
@@ -14,6 +14,13 @@
  * EDDiscovery is not affiliated with Frontier Developments plc.
  */
 using EDDiscovery.DB;
+using EDDiscovery.EDDN;
+using EDDiscovery.EDSM;
+using EDDiscovery.EliteDangerous;
+using EDDiscovery.EliteDangerous.JournalEvents;
+using EDDiscovery.Forms;
+using EDDiscovery.HTTP;
+using EDDiscovery.Win32Constants;
 using EDDiscovery2;
 using EDDiscovery2.DB;
 using EDDiscovery2.EDSM;
@@ -21,6 +28,8 @@ using EDDiscovery2.Forms;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
@@ -30,44 +39,18 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
-using System.Threading;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using System.Configuration;
-using EDDiscovery.EDSM;
-using System.Threading.Tasks;
 using System.Text;
-using System.ComponentModel;
 using System.Text.RegularExpressions;
-using EDDiscovery.HTTP;
-using EDDiscovery.Forms;
-using EDDiscovery.EliteDangerous;
-using EDDiscovery.EliteDangerous.JournalEvents;
-using EDDiscovery.EDDN;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EDDiscovery
 {
     public partial class EDDiscoveryForm : Form, IDiscoveryController
     {
         #region Variables
-
-        public const int WM_MOVE = 3;
-        public const int WM_SIZE = 5;
-        public const int WM_MOUSEMOVE = 0x200;
-        public const int WM_LBUTTONDOWN = 0x201;
-        public const int WM_LBUTTONUP = 0x202;
-        public const int WM_NCLBUTTONDOWN = 0xA1;
-        public const int WM_NCLBUTTONUP = 0xA2;
-        public const int WM_NCMOUSEMOVE = 0xA0;
-        public const int HT_CLIENT = 0x1;
-        public const int HT_CAPTION = 0x2;
-        public const int HT_LEFT = 0xA;
-        public const int HT_RIGHT = 0xB;
-        public const int HT_BOTTOM = 0xF;
-        public const int HT_BOTTOMRIGHT = 0x11;
-        public const int WM_NCL_RESIZE = 0x112;
-        public const int HT_RESIZE = 61448;
-        public const int WM_NCHITTEST = 0x84;
 
         // Mono compatibility
         private bool _window_dragging = false;
@@ -938,7 +921,7 @@ namespace EDDiscovery
         protected override void WndProc(ref Message m)
         {
             // Compatibility movement for Mono
-            if (m.Msg == WM_LBUTTONDOWN && (int)m.WParam == 1 && !theme.WindowsFrame)
+            if (m.Msg == WM.LBUTTONDOWN && (int)m.WParam == 1 && !theme.WindowsFrame)
             {
                 int x = unchecked((short)((uint)m.LParam & 0xFFFF));
                 int y = unchecked((short)((uint)m.LParam >> 16));
@@ -948,7 +931,7 @@ namespace EDDiscovery
                 m.Result = IntPtr.Zero;
                 this.Capture = true;
             }
-            else if (m.Msg == WM_MOUSEMOVE && (int)m.WParam == 1 && _window_dragging)
+            else if (m.Msg == WM.MOUSEMOVE && (int)m.WParam == 1 && _window_dragging)
             {
                 int x = unchecked((short)((uint)m.LParam & 0xFFFF));
                 int y = unchecked((short)((uint)m.LParam >> 16));
@@ -958,7 +941,7 @@ namespace EDDiscovery
                 this.Update();
                 m.Result = IntPtr.Zero;
             }
-            else if (m.Msg == WM_LBUTTONUP)
+            else if (m.Msg == WM.LBUTTONUP)
             {
                 _window_dragging = false;
                 _window_dragMousePos = Point.Empty;
@@ -967,12 +950,12 @@ namespace EDDiscovery
                 this.Capture = false;
             }
             // Windows honours NCHITTEST; Mono does not
-            else if (m.Msg == WM_NCHITTEST)
+            else if (m.Msg == WM.NCHITTEST)
             {
                 base.WndProc(ref m);
                 //System.Diagnostics.Debug.WriteLine( Environment.TickCount + " Res " + ((int)m.Result));
 
-                if ((int)m.Result == HT_CLIENT)
+                if ((int)m.Result == HT.CLIENT)
                 {
                     int x = unchecked((short)((uint)m.LParam & 0xFFFF));
                     int y = unchecked((short)((uint)m.LParam >> 16));
@@ -980,23 +963,23 @@ namespace EDDiscovery
 
                     if (p.X > this.ClientSize.Width - statusStrip1.Height && p.Y > this.ClientSize.Height - statusStrip1.Height)
                     {
-                        m.Result = (IntPtr)HT_BOTTOMRIGHT;
+                        m.Result = (IntPtr)HT.BOTTOMRIGHT;
                     }
                     else if (p.Y > this.ClientSize.Height - statusStrip1.Height)
                     {
-                        m.Result = (IntPtr)HT_BOTTOM;
+                        m.Result = (IntPtr)HT.BOTTOM;
                     }
                     else if (p.X > this.ClientSize.Width - 5)       // 5 is generous.. really only a few pixels gets thru before the subwindows grabs them
                     {
-                        m.Result = (IntPtr)HT_RIGHT;
+                        m.Result = (IntPtr)HT.RIGHT;
                     }
                     else if (p.X < 5)
                     {
-                        m.Result = (IntPtr)HT_LEFT;
+                        m.Result = (IntPtr)HT.LEFT;
                     }
                     else if (!theme.WindowsFrame)
                     {
-                        m.Result = (IntPtr)HT_CAPTION;
+                        m.Result = (IntPtr)HT.CAPTION;
                     }
                 }
             }
