02a406b207854334803cd97034244b604413f93a
Robert Wahlström <finwen_se@hotmail.com>
2017-02-27 14:42:44 +0100

    Added correct doc

--- EDDiscovery/EliteDangerous/JournalEvents/JornalMaterials.cs
+++ EDDiscovery/EliteDangerous/JournalEvents/JornalMaterials.cs
@@ -21,21 +21,14 @@ using System.Text;
 
 namespace EDDiscovery.EliteDangerous.JournalEvents
 {
-    //    When written: at startup, when loading from main menu
-    //Parameters:
-    //•	Modules: array of installed items, each with:
-    //o Slot: slot name
-    //o Item: module name
-    //o On: bool, indicates on or off
-    //o Priority: power priority
-    //o AmmoInClip: (if relevant)
-    //o AmmoInHopper: (if relevant)
-    //o EngineerBlueprint: blueprint name(if engineered)
-    //o EngineerLevel: blueprint level(if engineered)
-    //(For a passenger cabin, AmmoInClip holds the number of places in the cabin)
+//    When written: at startup, when loading from main menu into game
+//Parameters:
+//•	Raw: array of raw materials(each with name and count)
+//•	Manufactured: array of manufactured items
+//•	Encoded: array of scanned data
 
-    //Example:
-    //{ "timestamp":"2017-02-10T14:25:51Z", "event":"Loadout", "Modules":[ { "Slot":"HugeHardpoint1", "Item":"Hpt_MultiCannon_Gimbal_Huge", "On":true, "Priority":0, "AmmoInClip":90, "AmmoInHopper":2010 }, { "Slot":"MediumHardpoint1", "Item":"Hpt_BeamLaser_Turret_Medium", "On":true, "Priority":0 }, { "Slot":"MediumHardpoint2", "Item":"Hpt_BeamLaser_Turret_Medium", "On":true, "Priority":0, { "Slot":"TinyHardpoint1", "Item":"Hpt_PlasmaPointDefence_Turret_Tiny", "On":true, "Priority":0, "AmmoInClip":12, "AmmoInHopper":9940 }, { "Slot":"Armour", "Item":"FerDeLance_Armour_Grade1", "On":true, "Priority":1 }, { "Slot":"PaintJob", "Item":"PaintJob_FerDeLance_Tactical_White", "On":true, "Priority":1 }, { "Slot":"PowerPlant", "Item":"Int_Powerplant_Size6_Class5", "On":true, "Priority":1 }, { "Slot":"MainEngines", "Item":"Int_Engine_Size5_Class5", "On":true, "Priority":0 }, { "Slot":"FrameShiftDrive", "Item":"Int_Hyperdrive_Size4_Class5", "On":true, "Priority":0, "EngineerBlueprint":"FSD_LongRange", "EngineerLevel":5 }, { "Slot":"LifeSupport", "Item":"Int_LifeSupport_Size4_Class2", "On":true, "Priority":0 }, { "Slot":"PowerDistributor", "Item":"Int_PowerDistributor_Size6_Class5", "On":true, "Priority":0, "EngineerBlueprint":"PowerDistributor_PriorityWeapons", "EngineerLevel":1 }, { "Slot":"Radar", "Item":"Int_Sensors_Size4_Class5", 
+//Example:
+//{ "timestamp":"2017-02-10T14:25:51Z", "event":"Materials", "Raw":[ { "Name":"chromium", "Count":28 }, { "Name":"zinc", "Count":18 }, { "Name":"iron", "Count":23 }, { "Name":"sulphur", "Count":19 } ], "Manufactured":[ { "Name":"refinedfocuscrystals", "Count":10 }, { "Name":"highdensitycomposites", "Count":3 }, { "Name":"mechanicalcomponents", "Count":3 } ], "Encoded":[ { "Name":"emissiondata", "Count":32 }, { "Name":"shielddensityreports", "Count":23 } } ] }
 
     [JournalEntryType(JournalTypeEnum.Materials)]
     public class JornalMaterials : JournalEntry
