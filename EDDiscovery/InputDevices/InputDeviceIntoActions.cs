8c9ee06f4c760f630823d03ed546aa4e3389eb35
robbyxp1 <robxp@hotmail.com>
2017-05-16 22:02:34 +0100

    W2 refactored to work across devices

---
+++ EDDiscovery/InputDevices/InputDeviceIntoActions.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EDDiscovery.EliteDangerous;
+
+
+namespace EDDiscovery.InputDevices
+{
+    class InputDevicesIntoActions
+    {
+        InputDeviceList devices;
+        Actions.ActionController ac;
+        BindingsFile bf;
+        Timer t;
+
+        List<BindingsFile.Assignment> assignmentsinonstate = new List<BindingsFile.Assignment>();
+
+        public InputDevicesIntoActions(InputDeviceList pi, BindingsFile b , Actions.ActionController pc )
+        {
+            devices = pi;
+            bf = b;
+            ac = pc;
+        }
+
+        public void Start()
+        {
+            t = new Timer();
+            t.Interval = 100;
+            t.Tick += T_Tick;
+            t.Start();
+        }
+
+        BindingsFile.Device GetBindingDeviceFromInputDeviceIdentifier(InputDeviceIdentity i)
+        {
+            BindingsFile.Device dv = bf.FindDevice(i.Name, i.Instanceguid, i.Productguid);
+            return dv;
+        }
+
+        InputDeviceInterface GetInputDeviceFromBindingDevice(BindingsFile.Device dv)
+        {
+            InputDeviceInterface i = devices.inputdevices.Find(x => 
+                {
+                    BindingsFile.Device b = bf.FindDevice(x.ID().Name, x.ID().Instanceguid, x.ID().Productguid);
+                    return b != null && b.Name.Equals(dv.Name);
+                });
+
+            return i;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            List<InputDeviceEvent> list = devices.Poll();
+            foreach (InputDeviceEvent je in list)
+            {
+                string match = je.EventName();              // same as bindings name..
+                System.Diagnostics.Debug.WriteLine(je.ToString(10) + " " + match);
+
+                if (je.Pressed) // on event..
+                {
+                    BindingsFile.Device dv = GetBindingDeviceFromInputDeviceIdentifier(je.Device.ID());
+
+                    if (dv != null)
+                    {
+                        List<BindingsFile.Assignment> assignlist = dv.Find(match,false);
+
+                        if ( assignlist != null)
+                        {
+                            foreach(BindingsFile.Assignment a in assignlist)
+                            {
+                                if ( Verify(a))
+                                {
+                                    System.Diagnostics.Debug.WriteLine("  Pressed " + a.assignedfunc);
+                                    assignmentsinonstate.Add(a);
+                                }
+                            }
+
+                        }
+                    }
+                }
+                else
+                {               // off event..
+                    List<BindingsFile.Assignment> updatedassignmentsinonstate = new List<BindingsFile.Assignment>();
+
+                    foreach(BindingsFile.Assignment a in assignmentsinonstate)
+                    {
+                        if (!Verify(a))       // Need to verify all..
+                        {
+                            System.Diagnostics.Debug.WriteLine("  Released " + a.assignedfunc);
+                        }
+                        else
+                            updatedassignmentsinonstate.Add(a);
+                    }
+
+                    assignmentsinonstate = updatedassignmentsinonstate;
+                }
+            }
+        }
+
+        public bool Verify(BindingsFile.Assignment a)
+        {
+            foreach (BindingsFile.DeviceKeyPair ma in a.keys)
+            {
+                InputDeviceInterface idi = GetInputDeviceFromBindingDevice(ma.Device);
+
+                if (idi == null || !idi.IsPressed(ma.Key))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public void Stop()
+        {
+            t.Stop();
+        }
+
+        public void Dispose()
+        {
+            t.Stop();
+            devices.Dispose();
+        }
+    }
+}
---
+++ EDDiscovery/InputDevices/InputDeviceList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDDiscovery.InputDevices
+{
+    class InputDeviceList
+    {
+        public List<InputDeviceInterface> inputdevices { get; private set; } = new List<InputDeviceInterface>();
+
+        public void Add(InputDeviceInterface i)
+        {
+            inputdevices.Add(i);
+        }
+
+        public void Dispose()
+        {
+            foreach (InputDeviceInterface i in inputdevices)
+                i.Dispose();
+        }
+
+        public List<InputDeviceEvent> Poll()
+        {
+            List<InputDeviceEvent> list = new List<InputDeviceEvent>();
+            foreach (InputDeviceInterface i in inputdevices)
+            {
+                List<InputDeviceEvent> ilist = i.Poll();
+                if (ilist != null)
+                    list.AddRange(ilist);
+            }
+
+            return list;
+        }
+    }
+}
