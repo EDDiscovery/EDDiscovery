aa7e9a6f6367677ea74bae56f6ae91868d28caf4
Robert Wahlström <rw@unipower.se>
2017-06-27 16:12:53 +0200

    Fixes to EnigineerContribution events.
    (cherry picked from commit bf4389213c372ab27810ad8543790c5a5390d018)

--- EDDiscovery/EliteDangerous/JournalEvents/JournalEngineerContribution.cs
+++ EDDiscovery/EliteDangerous/JournalEvents/JournalEngineerContribution.cs
@@ -19,31 +19,61 @@ using System.Linq;
 namespace EDDiscovery.EliteDangerous.JournalEvents
 {
     //{ "timestamp":"2017-06-27T01:32:36Z", "event":"EngineerContribution", "Engineer":"Liz Ryder", "Type":"Commodity", "Commodity":"landmines", "Quantity":200, "TotalQuantity":200 }
+    //{ "timestamp":"2017-06-27T03:02:37Z", "event":"EngineerContribution", "Engineer":"Tiana Fortune", "Type":"Materials", "Material":"decodedemissiondata", "Quantity":50, "TotalQuantity":50 }
 
-    [JournalEntryType(JournalTypeEnum.EngineerContribution)]
+[JournalEntryType(JournalTypeEnum.EngineerContribution)]
     public class JournalEngineerContribution : JournalEntry
     {
         public JournalEngineerContribution(JObject evt ) : base(evt, JournalTypeEnum.EngineerContribution)
         {
             Engineer = evt["Engineer"].Str();
             Type = evt["Type"].Str();
+
+            if (Type.Equals("Commodity") || Type.Equals("Materials"))
+                unknownType = false;
+            else
+                unknownType = true;
+
             Commodity = evt["Commodity"].Str();
+            Commodity = JournalFieldNaming.FDNameTranslation(Commodity);     // pre-mangle to latest names, in case we are reading old journal records
+            FriendlyCommodity = JournalFieldNaming.RMat(Commodity);
+
+            Material = evt["Material"].Str();
+            Material = JournalFieldNaming.FDNameTranslation(Material);     // pre-mangle to latest names, in case we are reading old journal records
+            FriendlyMaterial = JournalFieldNaming.RMat(Material);
+
             Quantity = evt["Quantity"].Int();
             TotalQuantity = evt["TotalQuantity"].Int();
         }
 
+        private bool unknownType;
         public string Engineer { get; set; }
         public string Type { get; set; }
+        public string FriendlyCommodity { get; set; }
         public string Commodity { get; set; }
+        public string FriendlyMaterial { get; set; }
+        public string Material { get; set; }
         public int Quantity { get; set; }
         public int TotalQuantity { get; set; }
 
-        public override System.Drawing.Bitmap Icon { get { return EDDiscovery.Properties.Resources.engineerapply; } }
+        public override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                if (unknownType)
+                    return EDDiscovery.Properties.Resources.genericevent;
+                else
+                    return EDDiscovery.Properties.Resources.engineerapply;
+            }
+        }
 
         public override void FillInformation(out string summary, out string info, out string detailed) //V
         {
             summary = EventTypeStr.SplitCapsWord();
-            info = Tools.FieldBuilder("", Engineer, "Type:", Type, "Commodity:", Commodity, "Quantity:", Quantity, "TotalQuantity:", TotalQuantity);
+            if (unknownType==true)
+                info = "Report to EDDiscovery team an unknown EngineerContribution type: " + Type;
+             else
+                info = Tools.FieldBuilder("", Engineer, "Type:", Type, "Commodity:", FriendlyCommodity, "Material:", FriendlyMaterial, "Quantity:", Quantity, "TotalQuantity:", TotalQuantity);
             detailed = "";
         }
     }
