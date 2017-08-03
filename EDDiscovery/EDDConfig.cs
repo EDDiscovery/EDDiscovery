31c1b0fcb49c61da0e86e73b279bb8728f515ec4
Ben Peddell <klightspeed@killerwolves.net>
2017-08-03 23:01:01 +1000

    Add options to move databases
    Uses redirect files in data directory to redirect
    database files.

--- EDDiscovery/EDDConfig.cs
+++ EDDiscovery/EDDConfig.cs
@@ -353,6 +353,8 @@ namespace EDDiscovery
             public string UserDatabasePath { get; private set; }
             public string SystemDatabasePath { get; private set; }
             public string OldDatabasePath { get; private set; }
+            public string NewUserDatabasePath { get; private set; }
+            public string NewSystemDatabasePath { get; private set; }
             public bool StoreDataInProgramDirectory { get; private set; }
             public bool NoWindowReposition { get; private set; }
             public bool ActionButton { get; private set; }
@@ -457,6 +459,59 @@ namespace EDDiscovery
                 }
             }
 
+            private bool TryReadLine(string filename, out string line)
+            {
+                line = null;
+
+                if (File.Exists(filename))
+                {
+                    try
+                    {
+                        line = File.ReadLines(filename).FirstOrDefault();
+                        return true;
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                return false;
+            }
+
+            private void ProcessDbRedirectFiles()
+            {
+                string userdbpath = null;
+                string userdbmove = null;
+                string sysdbpath = null;
+                string sysdbmove = null;
+
+                if (UserDatabasePath == null)
+                {
+                    if (TryReadLine(Path.Combine(AppDataDirectory, "EDDUser.sqlite.redirect.new"), out userdbmove))
+                    {
+                        NewUserDatabasePath = userdbmove;
+                    }
+
+                    if (TryReadLine(Path.Combine(AppDataDirectory, "EDDUser.sqlite.redirect"), out userdbpath) && File.Exists(userdbpath))
+                    {
+                        UserDatabasePath = userdbpath;
+                    }
+                }
+
+                if (SystemDatabasePath == null)
+                {
+                    if (TryReadLine(Path.Combine(AppDataDirectory, "EDDSystem.sqlite.redirect.new"), out sysdbmove))
+                    {
+                        NewSystemDatabasePath = sysdbmove;
+                    }
+
+                    if (TryReadLine(Path.Combine(AppDataDirectory, "EDDSystem.sqlite.redirect"), out sysdbpath) && File.Exists(sysdbpath))
+                    {
+                        SystemDatabasePath = sysdbpath;
+                    }
+                }
+            }
+
             private void ProcessCommandLineOptions(Func<string, string, bool> getopt)
             {
                 string[] cmdlineopts = Environment.GetCommandLineArgs().ToArray();
@@ -514,6 +569,16 @@ namespace EDDiscovery
                     OldDatabasePath = optval;
                     return true;
                 }
+                else if (optname == "-movesystemsdb")
+                {
+                    NewSystemDatabasePath = optval;
+                    return true;
+                }
+                else if (optname == "-moveuserdb")
+                {
+                    NewUserDatabasePath = optval;
+                    return true;
+                }
                 else if (optname.StartsWith("-"))
                 {
                     string opt = optname.Substring(1).ToLowerInvariant();
@@ -555,11 +620,96 @@ namespace EDDiscovery
                 ProcessCommandLineOptions();
                 SetAppDataDirectory(AppFolder, StoreDataInProgramDirectory);
                 SetVersionDisplayString();
+                ProcessDbRedirectFiles();
                 if (UserDatabasePath == null) UserDatabasePath = Path.Combine(AppDataDirectory, "EDDUser.sqlite");
                 if (SystemDatabasePath == null) SystemDatabasePath = Path.Combine(AppDataDirectory, "EDDSystem.sqlite");
                 if (OldDatabasePath == null) OldDatabasePath = Path.Combine(AppDataDirectory, "EDDiscovery.sqlite");
 
                 EliteDangerousCore.EliteConfigInstance.InstanceOptions = this;
+            }
+
+            public bool MoveDatabases(Action<string> msg)
+            {
+                FileStream userdbstream = null;
+                FileStream newuserdbstream = null;
+                FileStream sysdbstream = null;
+                FileStream newsysdbstream = null;
+
+                try
+                {
+                    if (NewUserDatabasePath != null)
+                    {
+                        if (File.Exists(UserDatabasePath) && NewUserDatabasePath != UserDatabasePath)
+                        {
+                            FileStream instream = userdbstream = File.Open(UserDatabasePath, FileMode.Open, FileAccess.Read, FileShare.Delete);
+                            FileStream outstream = newuserdbstream = File.Open(NewUserDatabasePath, FileMode.CreateNew, FileAccess.Write, FileShare.None);
+                            long size = instream.Length;
+                            int blk = 0;
+                            int len = 0;
+                            byte[] data = new byte[65536];
+
+                            while ((len = instream.Read(data, 0, data.Length)) != 0)
+                            {
+                                outstream.Write(data, 0, len);
+                                blk++;
+                                if ((blk % 16) == 0)
+                                {
+                                    msg.Invoke($"Moving User DB ({outstream.Position / 1048576} / {size / 1048576}MiB)");
+                                }
+                            }
+                        }
+                    }
+
+                    if (NewSystemDatabasePath != null)
+                    {
+                        if (File.Exists(UserDatabasePath) && NewUserDatabasePath != UserDatabasePath)
+                        {
+                            FileStream instream = sysdbstream = File.Open(SystemDatabasePath, FileMode.Open, FileAccess.Read, FileShare.Delete);
+                            FileStream outstream = newsysdbstream = File.Open(NewSystemDatabasePath, FileMode.CreateNew, FileAccess.Write, FileShare.None);
+                            long size = instream.Length;
+                            int blk = 0;
+                            int len = 0;
+                            byte[] data = new byte[65536];
+
+                            while ((len = instream.Read(data, 0, data.Length)) != 0)
+                            {
+                                outstream.Write(data, 0, len);
+                                blk++;
+                                if ((blk % 16) == 0)
+                                {
+                                    msg.Invoke($"Moving System DB ({outstream.Position / 1048576} / {size / 1048576}MiB)");
+                                }
+                            }
+                        }
+                    }
+
+                    if (userdbstream != null && newuserdbstream != null && userdbstream.Length == newuserdbstream.Length)
+                    {
+                        File.WriteAllText(Path.Combine(AppDataDirectory, "EDDUser.sqlite.redirect"), NewUserDatabasePath);
+                        File.Delete(Path.Combine(AppDataDirectory, "EDDUser.sqlite.redirect.new"));
+                        File.Delete(UserDatabasePath);
+                        UserDatabasePath = NewUserDatabasePath;
+                        NewUserDatabasePath = null;
+                    }
+
+                    if (sysdbstream != null && newsysdbstream != null && sysdbstream.Length == newsysdbstream.Length)
+                    {
+                        File.WriteAllText(Path.Combine(AppDataDirectory, "EDDSystem.sqlite.redirect"), NewSystemDatabasePath);
+                        File.Delete(Path.Combine(AppDataDirectory, "EDDSystem.sqlite.redirect.new"));
+                        File.Delete(SystemDatabasePath);
+                        SystemDatabasePath = NewSystemDatabasePath;
+                        NewSystemDatabasePath = null;
+                    }
+
+                    return true;
+                }
+                finally
+                {
+                    if (newuserdbstream != null) newuserdbstream.Close();
+                    if (newsysdbstream != null) newsysdbstream.Close();
+                    if (userdbstream != null) userdbstream.Close();
+                    if (sysdbstream != null) sysdbstream.Close();
+                }
             }
         }
 
