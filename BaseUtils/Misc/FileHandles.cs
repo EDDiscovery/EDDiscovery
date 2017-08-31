a08b1db80eb4d171fa937fa22e0d0ab3039e479a
Robert Wahlström <finwen_se@hotmail.com>
2017-08-31 18:07:40 +0200

    Missed files

---
+++ BaseUtils/Misc/FileHandles.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseUtils
+{
+    public class FileHandle
+    {
+        StreamReader reader;
+        StreamWriter writer;
+
+        public bool Open(string f, FileMode fm, FileAccess ac, out string errmsg)
+        {
+            try
+            {
+                FileStream file = File.Open(f, fm, ac);
+
+                if (ac == FileAccess.Read)
+                    reader = new StreamReader(file);
+                else
+                    writer = new StreamWriter(file);
+
+                errmsg = "";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errmsg = ex.Message;
+                return false;
+            }
+        }
+
+        public void Close()
+        {
+            try
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                }
+                if (writer != null)
+                {
+                    writer.Close();
+                    writer.Dispose();
+                }
+            }
+            catch { }
+        }
+
+        public bool ReadLine(out string output)
+        {
+            try
+            {
+                if (reader != null)
+                {
+                    output = reader.ReadLine();
+                    return true;
+                }
+                else
+                {
+                    output = "Not open for read";
+                    return false;
+                }
+            }
+            catch
+            {
+                output = "Failed to read file";
+                return false;
+            }
+        }
+
+        public bool WriteLine(string value, bool lf, out string errmsg)
+        {
+            try
+            {
+                if (writer != null)
+                {
+                    if (lf)
+                        writer.WriteLine(value);
+                    else
+                        writer.Write(value);
+
+                    errmsg = "";
+                    return true;
+                }
+                else
+                {
+                    errmsg = "Not open for write";
+                    return false;
+                }
+            }
+            catch
+            {
+                errmsg = "Failed to read file";
+                return false;
+            }
+        }
+
+        public bool Seek(long value, out string errmsg)
+        {
+            try
+            {
+                if (writer != null)
+                    writer.BaseStream.Seek(value, SeekOrigin.Begin);
+                else
+                {
+                    reader.DiscardBufferedData();
+                    reader.BaseStream.Seek(value, SeekOrigin.Begin);
+                }
+                errmsg = "";
+                return true;
+            }
+            catch
+            {
+                errmsg = "Failed to read file";
+                return false;
+            }
+        }
+
+        public bool Tell(long value, out string output)
+        {
+            try
+            {
+                if (writer != null)
+                    output = writer.BaseStream.Position.ToString(System.Globalization.CultureInfo.InvariantCulture);
+                else
+                    output = reader.BaseStream.Position.ToString(System.Globalization.CultureInfo.InvariantCulture);
+
+                return true;
+            }
+            catch
+            {
+                output = "Failed to tell file position";
+                return false;
+            }
+        }
+    }
+
+    public class FileHandles
+    {
+        private Dictionary<int, FileHandle> handles;
+        private int nexthid;
+
+        public FileHandles()
+        {
+            nexthid = 1;
+            handles = new Dictionary<int, FileHandle>();
+        }
+
+        public void CloseAll()
+        {
+            foreach (int i in handles.Keys)
+            {
+                handles[i].Close();
+            }
+
+            handles.Clear();
+        }
+
+        public int Open(string f, FileMode fm, FileAccess ac, out string errmsg)        // 0 is bad!
+        {
+            FileHandle cfh = new FileHandle();
+            if (cfh.Open(f, fm, ac, out errmsg))
+            {
+                handles[nexthid] = cfh;
+                return nexthid++;
+            }
+            else
+                return 0;
+        }
+
+        public void Close(int h)
+        {
+            if (handles.ContainsKey(h))
+            {
+                handles[h].Close();
+                handles.Remove(h);
+            }
+        }
+
+        public bool ReadLine(int h, out string output)
+        {
+            if (handles.ContainsKey(h))
+                return handles[h].ReadLine(out output);
+            else
+            {
+                output = "Handle not found";
+                return false;
+            }
+        }
+
+        public bool WriteLine(int h, string value, bool lf, out string output)
+        {
+            if (handles.ContainsKey(h))
+                return handles[h].WriteLine(value, lf, out output);
+            else
+            {
+                output = "Handle not found";
+                return false;
+            }
+        }
+
+        public bool Seek(int h, long value, out string output)
+        {
+            if (handles.ContainsKey(h))
+                return handles[h].Seek(value, out output);
+            else
+            {
+                output = "Handle not found";
+                return false;
+            }
+        }
+
+        public bool Tell(int h, out string output)
+        {
+            if (handles.ContainsKey(h))
+                return handles[h].Tell(h, out output);
+            else
+            {
+                output = "Handle not found";
+                return false;
+            }
+        }
+    }
+}
---
+++ ExtendedControls/Forms/SmartSysMenuForm.cs
@@ -0,0 +1,175 @@
+﻿/*
+ * Copyright © 2017 EDDiscovery development team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * 
+ * EDDiscovery is not affiliated with Frontier Developments plc.
+ */
+using BaseUtils.Win32;
+using BaseUtils.Win32Constants;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtendedControls
+{
+    /// <summary>
+    /// A customized form offering a fully functional System Menu for everyone. <see cref="WM.SYSCOMMAND"/> values from
+    /// 0x0010 to 0xEFFF are availble for inherited classes to utilize.
+    /// values from 0x0001 to 0x000F, leaving inheritors free to work in between 0x0010 to 0x00FF.
+    /// </summary>
+    public class SmartSysMenuForm : Form
+    {
+        // TODO: managed wrapper to keep track of these all up and down the inhertance stack.
+        protected const int SC_ONTOP = 0x0001;
+        protected const int SC_OPACITYSUBMENU = 0x0002;    // 100% = 0x3; 90% = 0x4; ...; 10% = 0xC; 0% = NOT USED!
+        // 0x000D-0x000F are reserved by us for future expansion, while 0x0000 and 0xF000+ are system reserved.
+
+        protected virtual bool AllowResize { get; } = true;
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                if (_SysMenuCreationHackEnabled) cp.Style |= WS.SYSMENU;
+                return cp;
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                UnsafeNativeMethods.GetSystemMenu(Handle, true);
+                IntPtr hSysMenu = UnsafeNativeMethods.GetSystemMenu(Handle, false);
+                if (hSysMenu != IntPtr.Zero)
+                {
+                    UnsafeNativeMethods.AppendMenu(hSysMenu, MF.SEPARATOR, 0, string.Empty);
+                    UnsafeNativeMethods.AppendMenu(hSysMenu, MF.STRING, SC_ONTOP, "On &Top");
+
+                    IntPtr hOpacSubmenu = UnsafeNativeMethods.CreateMenu();
+                    if (hOpacSubmenu != IntPtr.Zero)
+                    {
+                        for (int i = 10; i > 0; i--)
+                            UnsafeNativeMethods.AppendMenu(hOpacSubmenu, MF.STRING, SC_OPACITYSUBMENU + i, $"{i / 10f:P0}");
+                        UnsafeNativeMethods.AppendMenu(hSysMenu, MF.STRING | MF.POPUP, (int)hOpacSubmenu, "&Opacity");
+                    }
+                }
+            }
+        }
+
+        protected IntPtr SendMessage(int msg, IntPtr wparam, IntPtr lparam)
+        {
+            Message message = Message.Create(this.Handle, msg, wparam, lparam);
+            this.WndProc(ref message);
+            return message.Result;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM.CREATE:         // Win32: set _SysMenuCreationHackEnabled to ensure that we get a system menu at startup.
+                    {
+                        _SysMenuCreationHackEnabled = Environment.OSVersion.Platform == PlatformID.Win32NT;
+                        base.WndProc(ref m);
+                        _SysMenuCreationHackEnabled = false;
+                        return;
+                    }
+
+                case WM.DESTROY:        // Win32: certain events (changing ShowInTaskBar) may destroy us. Re-enable the hack.
+                    {
+                        _SysMenuCreationHackEnabled = Environment.OSVersion.Platform == PlatformID.Win32NT;
+                        break;
+                    }
+
+                case WM.INITMENU:       // Win32: refresh the system menu before displaying it.
+                    {
+                        base.WndProc(ref m);    // Base should always get first crack at this.
+
+                        if (m.WParam != IntPtr.Zero && Environment.OSVersion.Platform == PlatformID.Win32NT && IsHandleCreated)
+                        {
+                            bool maximized = WindowState == FormWindowState.Maximized;
+                            // these don't matter too much, but it helps the user to know if something isn't allowed. WM_SYSCOMMAND is where we verify.
+                            UnsafeNativeMethods.EnableMenuItem(m.WParam, SC.RESTORE,  AllowResize &&  maximized ? MF.ENABLED : MF.GRAYED);
+                            UnsafeNativeMethods.EnableMenuItem(m.WParam, SC.SIZE,     AllowResize && !maximized ? MF.ENABLED : MF.GRAYED);
+                            UnsafeNativeMethods.EnableMenuItem(m.WParam, SC.MINIMIZE, AllowResize               ? MF.ENABLED : MF.GRAYED);
+                            UnsafeNativeMethods.EnableMenuItem(m.WParam, SC.MAXIMIZE, AllowResize && !maximized ? MF.ENABLED : MF.GRAYED);
+
+                            if (FormBorderStyle == FormBorderStyle.None) // base.WndProc() is useless...
+                            {
+                                UnsafeNativeMethods.EnableMenuItem(m.WParam, SC.MOVE, maximized ? MF.GRAYED : MF.ENABLED);
+                                UnsafeNativeMethods.EnableMenuItem(m.WParam, SC.CLOSE, MF.ENABLED);
+                            }
+
+                            UnsafeNativeMethods.ModifyMenu(m.WParam, SC_ONTOP, MF.BYCOMMAND | (TopMost ? MF.CHECKED : MF.UNCHECKED), SC_ONTOP, "On &Top");
+                            int opac = (int)Math.Min(10, Math.Round(Opacity * 10));  // 0.000-1.00 => 10-100
+                            for (int i = 10; i > 0; i--)
+                            {
+                                UnsafeNativeMethods.ModifyMenu(m.WParam, SC_OPACITYSUBMENU + i, MF.BYCOMMAND | (opac == i ? MF.CHECKED : MF.UNCHECKED), SC_OPACITYSUBMENU + i, $"{i / 10f:P0}");
+                            }
+
+                            // This only works reliably on the application's MainForm.
+                            UnsafeNativeMethods.SetMenuDefaultItem(m.WParam, maximized && AllowResize ? SC.RESTORE : (!maximized && AllowResize ? SC.MAXIMIZE : SC.CLOSE), 0);
+                            m.Result = IntPtr.Zero;
+                        }
+                        return;
+                    }
+
+                case WM.NCRBUTTONUP:    // Win32: Display the system menu.
+                    {
+                        if (FormBorderStyle == FormBorderStyle.None && m.WParam == (IntPtr)HT.CAPTION && Environment.OSVersion.Platform == PlatformID.Win32NT && IsHandleCreated)
+                        {
+                            Point p = new Point((int)m.LParam);
+                            var hMenu = UnsafeNativeMethods.GetSystemMenu(Handle, false);
+                            if (hMenu != IntPtr.Zero)
+                            {
+                                int cmd = UnsafeNativeMethods.TrackPopupMenuEx(hMenu, UnsafeNativeMethods.GetSystemMetrics(SystemMetrics.MENUDROPALIGNMENT) | TPM.RETURNCMD, p.X, p.Y, Handle, IntPtr.Zero);
+                                if (cmd != 0)
+                                    UnsafeNativeMethods.PostMessage(Handle, WM.SYSCOMMAND, (IntPtr)cmd, IntPtr.Zero);
+                            }
+                            m.Result = IntPtr.Zero;
+                            return;
+                        }
+                        break;
+                    }
+
+                case WM.SYSCOMMAND:     // Process any system commands intended for this window (SC_ONTOP / SC_OPACITYSUBMENU).
+                    {
+                        if (m.WParam == (IntPtr)SC_ONTOP)
+                            TopMost = !TopMost;
+                        else if ((int)m.WParam > SC_OPACITYSUBMENU && (int)m.WParam <= SC_OPACITYSUBMENU + 10)
+                            Opacity = ((int)m.WParam - SC_OPACITYSUBMENU) / 10f;
+                        else if (!AllowResize && (m.WParam == (IntPtr)SC.MAXIMIZE || m.WParam == (IntPtr)SC.SIZE || m.WParam == (IntPtr)SC.RESTORE))
+                            return;     // Access Denied.
+                        else
+                            break;
+
+                        m.Result = IntPtr.Zero;
+                        return;
+                    }
+            }
+            base.WndProc(ref m);
+        }
+
+        // If WS.SYSMENU is not active at first WM.CREATE, the menu will not be created. Since FormBorderStyle may clear WS.SYSMENu, we have
+        // to fake it during startup. WS.SYSMENU is meaningless to us outside of WM.CREATE. Seealso https://stackoverflow.com/a/16695606
+        // CAUTION: if WS.SYSMENU is enabled but WS.CAPTION is not, all hittests, including our sysmenu, min/max/close, etc., will get ignored!
+        private bool _SysMenuCreationHackEnabled = Environment.OSVersion.Platform == PlatformID.Win32NT;
+    }
+}
