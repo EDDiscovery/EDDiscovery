17a120fda0f91874ece32834b9a51ff4427ed6af
phroggie <phroggster@gmail.com>
2017-09-02 04:28:35 -0500

    Undo NUnit bump, Cleanup Tests (Remove all MSTest)
    This functions well in Visual Studio 2015, 2017, AppVeyor, and
    Travis-CI. Leaner and cleaner.

--- EDDiscoveryTests/TravelHistoryFilterTests.cs
+++ EDDiscoveryTests/TravelHistoryFilterTests.cs
@@ -13,36 +13,22 @@
  * 
  * EDDiscovery is not affiliated with Frontier Developments plc.
  */
+using EDDiscovery;
+using EDDiscovery.DB;
+using EDDiscovery.UserControls;
+using EliteDangerous;
+using EliteDangerousCore;
+using NFluent;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using EDDiscovery;
-using NFluent;
-using EDDiscovery.DB;
 
 namespace EDDiscoveryTests
 {
-    using EliteDangerous;
-    using EDDiscovery.UserControls;
-#if false
-    // Visual Studio Test Framework
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-    partial class TestFixtureAttribute : Attribute { }
-    partial class TestAttribute : Attribute { }
-#else
-    // NUnit Test Framework
-    using NUnit.Framework;
-    using EliteDangerousCore;
-
-    partial class TestClassAttribute : Attribute { }
-    partial class TestMethodAttribute : Attribute { }
-#endif
-
-    [TestFixture]
-    [TestClass]
+    [TestFixture(TestOf = typeof(TravelHistoryFilter))]
     public class TravelHistoryFilterTests
     {
-        private ISystem sol = new EliteDangerousCore.SystemClass
+        private readonly ISystem sol = new EliteDangerousCore.SystemClass
         {
             name = "Sol",
             x = 0,
@@ -51,7 +37,6 @@ namespace EDDiscoveryTests
         };
 
         [Test]
-        [TestMethod]
         public void No_filter_does_not_filter_anything()
         {
             var veryOldData = HistoryEntry.MakeVSEntry(sol, DateTime.UtcNow.Subtract(TimeSpan.FromDays(500000)), 0, "", "");
@@ -61,7 +46,6 @@ namespace EDDiscoveryTests
         }
 
         [Test]
-        [TestMethod]
         public void Data_age_filter_removes_data_older_than_the_limit_and_keeps_data_more_recent_than_the_limit()
         {
             var now = HistoryEntry.MakeVSEntry(sol, DateTime.UtcNow, 0, "", "");
@@ -72,7 +56,6 @@ namespace EDDiscoveryTests
         }
 
         [Test]
-        [TestMethod]
         public void Last_2_items_filter_returns_the_2_most_recent_items_sorted_by_most_recent_and_removes_the_older_items()
         {
             var twentyDaysAgo = HistoryEntry.MakeVSEntry(sol, DateTime.UtcNow.Subtract(TimeSpan.FromDays(20)), 0, "", "");
@@ -84,49 +67,42 @@ namespace EDDiscoveryTests
         }
 
         [Test]
-        [TestMethod]
         public void No_filter_has_correct_label()
         {
             Check.That(TravelHistoryFilter.NoFilter.Label).IsEqualTo("All");
         }
 
         [Test]
-        [TestMethod]
         public void last_20_filter_has_correct_label()
         {
             Check.That(TravelHistoryFilter.Last(20).Label).IsEqualTo("Last 20 entries");
         }
 
         [Test]
-        [TestMethod]
         public void Last_6_hours_filter_has_correct_label()
         {
             Check.That(TravelHistoryFilter.FromHours(6).Label).IsEqualTo("6 hours");
         }
 
         [Test]
-        [TestMethod]
         public void Last_days_filter_has_correct_label()
         {
             Check.That(TravelHistoryFilter.FromDays(3).Label).IsEqualTo("3 days");
         }
 
         [Test]
-        [TestMethod]
         public void Last_week_filter_has_correct_label()
         {
             Check.That(TravelHistoryFilter.FromWeeks(1).Label).IsEqualTo("One Week");
         }
 
         [Test]
-        [TestMethod]
         public void Last_3_weeks_filter_has_correct_label()
         {
             Check.That(TravelHistoryFilter.FromWeeks(3).Label).IsEqualTo("3 weeks");
         }
 
         [Test]
-        [TestMethod]
         public void Last_month_filter_has_correct_label()
         {
             Check.That(TravelHistoryFilter.LastMonth().Label).IsEqualTo("Month");
