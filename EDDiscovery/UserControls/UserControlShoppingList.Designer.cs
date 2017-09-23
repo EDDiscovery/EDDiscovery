2f3c8c5e2af8f13bbf543fe64028654ec08b3a21
Iain Ross <id_ross@hotmail.com>
2017-09-23 20:15:32 +0100

    Added new control

---
+++ EDDiscovery/UserControls/UserControlShoppingList.Designer.cs
@@ -0,0 +1,163 @@
+﻿/*
+ * Copyright © 2016 - 2017 EDDiscovery development team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * 
+ * EDDiscovery is not affiliated with Frontier Developments plc.
+ */
+namespace EDDiscovery.UserControls
+{
+    partial class UserControlShoppingList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.splitContainer1 = new ExtendedControls.SplitContainerCustom();
+            this.splitContainer2 = new ExtendedControls.SplitContainerCustom();
+            this.pictureBoxList = new ExtendedControls.PictureBoxHotspot();
+            this.userControlSynthesis = new EDDiscovery.UserControls.UserControlSynthesis();
+            this.userControlEngineering = new EDDiscovery.UserControls.UserControlEngineering();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ShowAlways = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBoxList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(971, 572);
+            this.splitContainer1.SplitterDistance = 323;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.userControlSynthesis);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.userControlEngineering);
+            this.splitContainer2.Size = new System.Drawing.Size(644, 572);
+            this.splitContainer2.SplitterDistance = 214;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // pictureBoxList
+            // 
+            this.pictureBoxList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxList.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxList.Name = "pictureBoxList";
+            this.pictureBoxList.Size = new System.Drawing.Size(320, 569);
+            this.pictureBoxList.TabIndex = 0;
+            // 
+            // userControlSynthesis
+            // 
+            this.userControlSynthesis.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userControlSynthesis.Location = new System.Drawing.Point(0, 0);
+            this.userControlSynthesis.Name = "userControlSynthesis";
+            this.userControlSynthesis.Size = new System.Drawing.Size(641, 211);
+            this.userControlSynthesis.TabIndex = 0;
+            // 
+            // userControlEngineering
+            // 
+            this.userControlEngineering.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userControlEngineering.Location = new System.Drawing.Point(0, -1);
+            this.userControlEngineering.Name = "userControlEngineering";
+            this.userControlEngineering.Size = new System.Drawing.Size(641, 352);
+            this.userControlEngineering.TabIndex = 0;
+            // 
+            // UserControlShoppingList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "UserControlShoppingList";
+            this.Size = new System.Drawing.Size(971, 572);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip1;
+        private ExtendedControls.SplitContainerCustom splitContainer1;
+        private ExtendedControls.PictureBoxHotspot pictureBoxList;
+        private ExtendedControls.SplitContainerCustom splitContainer2;
+        private UserControlSynthesis userControlSynthesis;
+        private UserControlEngineering userControlEngineering;
+    }
+}
---
+++ EDDiscovery/UserControls/UserControlShoppingList.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Copyright © 2016 - 2017 EDDiscovery development team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * 
+ * EDDiscovery is not affiliated with Frontier Developments plc.
+ */
+using EDDiscovery.Controls;
+using EliteDangerousCore;
+using EliteDangerousCore.DB;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EDDiscovery.UserControls
+{
+    public partial class UserControlShoppingList : UserControlCommonBase
+    {
+        private int displaynumber = 0;
+        private EDDiscoveryForm discoveryform;
+        private UserControlCursorType uctg;
+
+        #region Init
+
+        public UserControlShoppingList()
+        {
+            InitializeComponent();
+        }
+
+        public override void Init(EDDiscoveryForm ed, UserControlCursorType thc, int vn) //0=primary, 1 = first windowed version, etc
+        {
+            discoveryform = ed;
+            uctg = thc;
+            displaynumber = vn;
+            //need to do something for vn in child controls so it won't conflict with standalone ones.  +20 will work for now, but could be cleaner
+            userControlEngineering.Init(ed, thc, vn+20);
+            userControlSynthesis.Init(ed, thc, vn+20);
+            
+        }
+
+        public override void ChangeCursorType(UserControlCursorType thc)
+        {
+            uctg.OnTravelSelectionChanged -= Display;
+            uctg = thc;
+            uctg.OnTravelSelectionChanged += Display;
+        }
+
+        #endregion
+
+        #region Display
+
+        public override void InitialDisplay()
+        {
+            last_he = uctg.GetCurrentHistoryEntry;
+            Display();
+        }
+
+        private void Discoveryform_OnNewEntry(HistoryEntry he, HistoryList hl)
+        {
+            last_he = he;
+            Display();
+        }
+
+        HistoryEntry last_he = null;
+        private void Display(HistoryEntry he, HistoryList hl)
+        {
+            last_he = he;
+            Display();
+        }
+
+        private void Display()
+        {
+
+            if (last_he != null)
+            {
+            }
+        }
+
+        #endregion
+
+        #region Layout
+
+        public override void LoadLayout()
+        {
+        }
+
+        public override void Closing()
+        {
+        }
+
+        #endregion
+
+        private void comboBoxHistoryWindow_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private Rectangle moveMoveDragBox;
+
+    }
+}
