96c0cc1a76f7bdf97b88f4480e100ba0e6f22d26
Ben Peddell <klightspeed@killerwolves.net>
2017-09-24 03:40:13 +1000

    Fix the ability to design the user controls
    Note that a rebuild of the solution followed by
    a restart of Visual Studio may be required.

---
+++ EDDiscovery/UserControls/AbstractControlDescriptionProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDDiscovery.UserControls
+{
+    // https://stackoverflow.com/questions/6817107/abstract-usercontrol-inheritance-in-visual-studio-designer#answer-17661386
+    public class AbstractControlDescriptionProvider<TAbstract, TBase> : TypeDescriptionProvider
+    {
+        public AbstractControlDescriptionProvider() : base(TypeDescriptor.GetProvider(typeof(TAbstract)))
+        {
+        }
+
+        public override Type GetReflectionType(Type objectType, object instance)
+        {
+            if (objectType == typeof(TAbstract))
+            {
+                return typeof(TBase);
+            }
+            else
+            {
+                return base.GetReflectionType(objectType, instance);
+            }
+        }
+
+        public override object CreateInstance(IServiceProvider provider, Type objectType, Type[] argTypes, object[] args)
+        {
+            if (objectType == typeof(TAbstract))
+            {
+                objectType = typeof(TBase);
+            }
+
+            return base.CreateInstance(provider, objectType, argTypes, args);
+        }
+    }
+}
