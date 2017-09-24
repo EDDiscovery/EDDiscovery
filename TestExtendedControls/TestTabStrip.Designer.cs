7a7820273f314eab0353aef79722d62115477cb6
robbyxp1 <robxp@hotmail.com>
2017-09-24 16:48:44 +0100

    W1 List control paints in both system and ext mode icons

--- TestExtendedControls/TestTabStrip.Designer.cs
+++ TestExtendedControls/TestTabStrip.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestTabStrip));
             this.panel1 = new System.Windows.Forms.Panel();
             this.tabStrip1 = new ExtendedControls.TabStrip();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listControlCustom1 = new ExtendedControls.ListControlCustom();
+            this.listControlCustom2 = new ExtendedControls.ListControlCustom();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listControlCustom3 = new ExtendedControls.ListControlCustom();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,11 +59,101 @@
             this.tabStrip1.StripAtTop = false;
             this.tabStrip1.TabIndex = 0;
             // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(45, 179);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(341, 190);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // listControlCustom1
+            // 
+            this.listControlCustom1.BorderColor = System.Drawing.Color.Red;
+            this.listControlCustom1.FitToItemsHeight = true;
+            this.listControlCustom1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.listControlCustom1.GradientColorScaling = 0.5F;
+            this.listControlCustom1.ImageItems = null;
+            this.listControlCustom1.ItemHeight = 20;
+            this.listControlCustom1.Items = ((System.Collections.Generic.List<string>)(resources.GetObject("listControlCustom1.Items")));
+            this.listControlCustom1.Location = new System.Drawing.Point(587, 179);
+            this.listControlCustom1.MouseOverBackgroundColor = System.Drawing.Color.Silver;
+            this.listControlCustom1.Name = "listControlCustom1";
+            this.listControlCustom1.ScrollBarButtonColor = System.Drawing.Color.LightGray;
+            this.listControlCustom1.ScrollBarColor = System.Drawing.Color.LightGray;
+            this.listControlCustom1.ScrollBarWidth = 16;
+            this.listControlCustom1.SelectedIndex = -1;
+            this.listControlCustom1.SelectionBackColor = System.Drawing.Color.Gray;
+            this.listControlCustom1.Size = new System.Drawing.Size(75, 143);
+            this.listControlCustom1.TabIndex = 2;
+            this.listControlCustom1.Text = "listControlCustom1";
+            // 
+            // listControlCustom2
+            // 
+            this.listControlCustom2.BorderColor = System.Drawing.Color.Red;
+            this.listControlCustom2.FitToItemsHeight = true;
+            this.listControlCustom2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.listControlCustom2.GradientColorScaling = 0.5F;
+            this.listControlCustom2.ImageItems = null;
+            this.listControlCustom2.ItemHeight = 20;
+            this.listControlCustom2.Items = ((System.Collections.Generic.List<string>)(resources.GetObject("listControlCustom2.Items")));
+            this.listControlCustom2.Location = new System.Drawing.Point(678, 179);
+            this.listControlCustom2.MouseOverBackgroundColor = System.Drawing.Color.Silver;
+            this.listControlCustom2.Name = "listControlCustom2";
+            this.listControlCustom2.ScrollBarButtonColor = System.Drawing.Color.LightGray;
+            this.listControlCustom2.ScrollBarColor = System.Drawing.Color.LightGray;
+            this.listControlCustom2.ScrollBarWidth = 16;
+            this.listControlCustom2.SelectedIndex = -1;
+            this.listControlCustom2.SelectionBackColor = System.Drawing.Color.Gray;
+            this.listControlCustom2.Size = new System.Drawing.Size(75, 143);
+            this.listControlCustom2.TabIndex = 2;
+            this.listControlCustom2.Text = "listControlCustom1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 30;
+            this.listBox1.Location = new System.Drawing.Point(430, 179);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(134, 154);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            // 
+            // listControlCustom3
+            // 
+            this.listControlCustom3.BorderColor = System.Drawing.Color.Red;
+            this.listControlCustom3.FitToItemsHeight = true;
+            this.listControlCustom3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.listControlCustom3.GradientColorScaling = 0.5F;
+            this.listControlCustom3.ImageItems = null;
+            this.listControlCustom3.ItemHeight = 26;
+            this.listControlCustom3.Items = ((System.Collections.Generic.List<string>)(resources.GetObject("listControlCustom3.Items")));
+            this.listControlCustom3.Location = new System.Drawing.Point(769, 179);
+            this.listControlCustom3.MouseOverBackgroundColor = System.Drawing.Color.Silver;
+            this.listControlCustom3.Name = "listControlCustom3";
+            this.listControlCustom3.ScrollBarButtonColor = System.Drawing.Color.LightGray;
+            this.listControlCustom3.ScrollBarColor = System.Drawing.Color.LightGray;
+            this.listControlCustom3.ScrollBarWidth = 16;
+            this.listControlCustom3.SelectedIndex = -1;
+            this.listControlCustom3.SelectionBackColor = System.Drawing.Color.Gray;
+            this.listControlCustom3.Size = new System.Drawing.Size(74, 143);
+            this.listControlCustom3.TabIndex = 2;
+            this.listControlCustom3.Text = "listControlCustom1";
+            // 
             // TestTabStrip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(926, 546);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listControlCustom2);
+            this.Controls.Add(this.listControlCustom3);
+            this.Controls.Add(this.listControlCustom1);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.panel1);
             this.Name = "TestTabStrip";
             this.Text = "TestTabControl";
@@ -70,5 +166,10 @@
 
         private System.Windows.Forms.Panel panel1;
         private ExtendedControls.TabStrip tabStrip1;
+        private System.Windows.Forms.ListView listView1;
+        private ExtendedControls.ListControlCustom listControlCustom1;
+        private ExtendedControls.ListControlCustom listControlCustom2;
+        private System.Windows.Forms.ListBox listBox1;
+        private ExtendedControls.ListControlCustom listControlCustom3;
     }
 }
--- TestExtendedControls/TestTabStrip.cs
+++ TestExtendedControls/TestTabStrip.cs
@@ -37,6 +37,102 @@ namespace DialogTest
                 "icon 18", "icon 19",
             };
             tabStrip1.SetControlText("Test tab");
+
+
+            ListViewItem item1 = new ListViewItem("item1", 0);
+            // Place a check mark next to the item.
+            item1.Checked = true;
+            item1.SubItems.Add("1");
+            item1.SubItems.Add("2");
+            item1.SubItems.Add("3");
+            ListViewItem item2 = new ListViewItem("item2", 1);
+            item2.SubItems.Add("4");
+            item2.SubItems.Add("5");
+            item2.SubItems.Add("6");
+            ListViewItem item3 = new ListViewItem("item3", 0);
+            // Place a check mark next to the item.
+            item3.Checked = true;
+            item3.SubItems.Add("7");
+            item3.SubItems.Add("8");
+            item3.SubItems.Add("9");
+
+            // Create columns for the items and subitems.
+            // Width of -2 indicates auto-size.
+            listView1.Columns.Add("Item Column", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 2", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 3", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 4", -2, HorizontalAlignment.Center);
+
+            //Add the items to the ListView.
+            listView1.Items.AddRange(new ListViewItem[] { item1, item2, item3 });
+
+            // Create two ImageList objects.
+            ImageList imageListSmall = new ImageList();
+            ImageList imageListLarge = new ImageList();
+
+            // Initialize the ImageList objects with bitmaps.
+            imageListSmall.Images.Add(DialogTest.Properties.Resources.galaxy_gray);
+            imageListSmall.Images.Add(DialogTest.Properties.Resources.galaxy_black);
+            imageListLarge.Images.Add(DialogTest.Properties.Resources.galaxy_gray);
+            imageListLarge.Images.Add(DialogTest.Properties.Resources.galaxy_gray);
+
+            //Assign the ImageList objects to the ListView.
+            listView1.LargeImageList = imageListLarge;
+            listView1.SmallImageList = imageListSmall;
+
+            // Add the ListView to the control
+            List<string> lv = new List<string>() { "one", "two", "three", "four" };
+            List<Image> lvimages = new List<Image>() { DialogTest.Properties.Resources.galaxy_gray, DialogTest.Properties.Resources.galaxy_black, DialogTest.Properties.Resources.galaxy_gray, DialogTest.Properties.Resources.galaxy_black };
+
+            listControlCustom1.Items = lv;
+            listControlCustom1.BackColor = Color.Black;
+            listControlCustom1.ForeColor = Color.Red;
+
+            listControlCustom2.Items = lv;
+            listControlCustom2.BackColor = Color.Black;
+            listControlCustom2.ForeColor = Color.Red;
+            listControlCustom2.FlatStyle = FlatStyle.Popup;
+            listControlCustom2.ImageItems = lvimages;
+
+            listControlCustom3.Items = lv;
+            listControlCustom3.ImageItems = lvimages;
+
+
+            listBox1.Items.AddRange(lv.ToArray());
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            // Define the default color of the brush as black.
+            Brush myBrush = Brushes.Black;
+
+            // Determine the color of the brush to draw each item based 
+            // on the index of the item to draw.
+            switch (e.Index)
+            {
+                case 0:
+                    myBrush = Brushes.Red;
+                    break;
+                case 1:
+                    myBrush = Brushes.Orange;
+                    break;
+                case 2:
+                    myBrush = Brushes.Purple;
+                    break;
+            }
+            Bitmap bmp = DialogTest.Properties.Resources.galaxy_gray;
+
+            e.Graphics.DrawImage(bmp, new Point(0, e.Bounds.Top));
+
+            Rectangle textarea = e.Bounds;
+            textarea.X += bmp.Width;
+            // Draw the current item text based on the current Font 
+            // and the custom brush settings.
+            e.Graphics.DrawString(listBox1.Items[e.Index].ToString(),
+                e.Font, myBrush, textarea, StringFormat.GenericDefault);
+            // If the ListBox has focus, draw a focus rectangle around the selected item.
+            e.DrawFocusRectangle();
         }
     }
 }
