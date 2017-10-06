89929a8c7311f465b24d338f52bde4aee448dab6
Ben Peddell <klightspeed@killerwolves.net>
2017-10-07 03:44:58 +1000

    Remove unneeded `using EDDiscovery.DB` from tests

--- EDDiscoveryTests/3DMap/DatasetBuilderTests.cs
+++ EDDiscoveryTests/3DMap/DatasetBuilderTests.cs
@@ -14,7 +14,6 @@
  * EDDiscovery is not affiliated with Frontier Developments plc.
  */
 using EDDiscovery;
-using EDDiscovery.DB;
 using EDDiscovery2._3DMap;
 using System;
 using System.Collections.Generic;
