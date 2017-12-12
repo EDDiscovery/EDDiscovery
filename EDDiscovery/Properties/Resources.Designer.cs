d99e40fed392e4ee1d37e9bcee12ea4cc089b748
Andrea Spada <spada.andrea@gmail.com>
2017-12-12 11:53:26 -0200

    Added a new panel, ScanGrid, which display the scanned bodies of a system in a grid, with some brief information.

--- EDDiscovery/Properties/Resources.Designer.cs
+++ EDDiscovery/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace EDDiscovery.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -599,6 +599,16 @@ namespace EDDiscovery.Properties {
         internal static System.Drawing.Bitmap save {
             get {
                 object obj = ResourceManager.GetObject("save", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap scangrid {
+            get {
+                object obj = ResourceManager.GetObject("scangrid", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
