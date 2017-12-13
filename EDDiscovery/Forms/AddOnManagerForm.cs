cd315e2152f8381e02fffc942c61d1e2f7f724a2
robbyxp1 <robxp@hotmail.com>
2017-12-13 13:25:43 +0000

    Slightly wider name display for file name for add on manager form

--- EDDiscovery/Forms/AddOnManagerForm.cs
+++ EDDiscovery/Forms/AddOnManagerForm.cs
@@ -181,9 +181,9 @@ namespace EDDiscovery.Forms
 
             int[] tabs;
             if ( managedownloadmode )
-                tabs = new int[] { 0, 100, 260, 340, 550, 730, 820, 900 };
+                tabs = new int[] { 0, 100, 300, 380, 590, 770, 860, 940 };
             else
-                tabs = new int[] { 0, 100, 260, 340, 550, 550, 700, 800 };
+                tabs = new int[] { 0, 100, 300, 380, 590, 590, 740, 840 };
 
             panelVScroll.Controls.Add(new Label() { Location = new Point(tabs[0] + panelleftmargin, panelheightmargin), Size = new Size(80, 24), Text = "Type" });
             panelVScroll.Controls.Add(new Label() { Location = new Point(tabs[1] + panelleftmargin, panelheightmargin), Size = new Size(80, 24), Text = "Name" });
