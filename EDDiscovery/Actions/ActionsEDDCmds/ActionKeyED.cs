f528e75325a56afab668572615a06dc43b5059d1
robbyxp1 <robxp@hotmail.com>
2017-12-21 11:58:27 +0000

    Changed phrase on bad SSML markup to indicate you can turn it off.
    Removed spurious var from keyed.

--- EDDiscovery/Actions/ActionsEDDCmds/ActionKeyED.cs
+++ EDDiscovery/Actions/ActionsEDDCmds/ActionKeyED.cs
@@ -54,8 +54,6 @@ namespace EDDiscovery.Actions
 
                         if ( matches != null )      // null if no matches to keyboard is found
                         {
-                            Tuple<Device, Assignment> match = matches[0];      // just use the first one.. since they have been prechecked for keys one is the same as another
-
                             // pick out the keys and convert them from fontier to Keys
                             Keys[] keys = (from x in matches[0].Item2.keys select DirectInputDevices.KeyConversion.FrontierNameToKeys(x.Key)).ToArray();
 
