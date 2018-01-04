9c55f616fd2625e86e058f2b9514f2d750402fdf
robbyxp1 <robxp@hotmail.com>
2018-01-04 11:10:57 +0000

    Re-ordered StarScan.cs for clarity
    Re-ordered for clarity, made sure only public IFs are there and
    annotated them.  No code changes (excepting the removal of public IF
    without EDSM, which can be emulated by calling the FindSystem with
    edsm=false)

--- EDDiscovery/DebugCode.cs
+++ EDDiscovery/DebugCode.cs
@@ -65,7 +65,7 @@ namespace EDDiscovery
                     }
                     else if (je.EventTypeID == EliteDangerousCore.JournalTypeEnum.Scan)
                     {
-                        ss.Process(je as EliteDangerousCore.JournalEvents.JournalScan, new EliteDangerousCore.SystemClass(system));
+                        //ss.Process(je as EliteDangerousCore.JournalEvents.JournalScan, new EliteDangerousCore.SystemClass(system));
                     }
                 }
 
