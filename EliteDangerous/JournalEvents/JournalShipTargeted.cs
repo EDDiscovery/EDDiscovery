6e3f3b2618bcb369227fed14ef49e19afb7993b5
robbyxp1 <robxp@hotmail.com>
2018-01-26 22:57:38 +0000

    Ship Targetting fields and Under Attack

--- EliteDangerous/JournalEvents/JournalShipTargeted.cs
+++ EliteDangerous/JournalEvents/JournalShipTargeted.cs
@@ -40,6 +40,13 @@ namespace EliteDangerousCore.JournalEvents
             PilotName = evt["PilotName"].StrNull();
             PilotName_Localised = evt["PilotName_Localised"].StrNull();
             PilotRank = evt["PilotRank"].StrNull();
+            ShieldHealth = evt["ShieldHealth"].DoubleNull();
+            HullHealth = evt["HullHealth"].DoubleNull();
+            Faction = evt["Faction"].StrNull();
+            LegalStatus = evt["LegalStatus"].StrNull();
+            Bounty = evt["Bounty"].IntNull();
+            SubSystem = evt["PilotRank"].StrNull();
+            SubSystemHealth = evt["SubSystemHealth"].DoubleNull();
 
             if (Ship != null)
                 Ship = JournalFieldNaming.GetBetterShipName(Ship);
@@ -52,11 +59,23 @@ namespace EliteDangerousCore.JournalEvents
         public string PilotName { get; set; }
         public string PilotName_Localised { get; set; }
         public string PilotRank { get; set; }
+        public double? ShieldHealth { get; set; }
+        public double? HullHealth { get; set; }
+        public string Faction { get; set; }
+        public string LegalStatus { get; set; }
+        public int? Bounty { get; set; }
+        public string SubSystem { get; set; }
+        public double? SubSystemHealth { get; set; }
 
         public override void FillInformation(out string summary, out string info, out string detailed) //V
         {
             summary = EventTypeStr.SplitCapsWord();
-            info = "";
+            if (TargetLocked)
+                info = BaseUtils.FieldBuilder.Build("", Ship_Localised.Alt(Ship), "", PilotName_Localised.Alt(PilotName),
+                            " Rank ", PilotRank, "Shield ;;0.0", ShieldHealth, "Hull ;;0.0", HullHealth, "Faction ", Faction,
+                            "", LegalStatus, "Bounty ", Bounty, "", SubSystem, "< at ;;0.0", SubSystemHealth);
+            else
+                info = "Lost Target";
             detailed = "";
         }
 
