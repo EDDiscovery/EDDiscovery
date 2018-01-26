0e7cf687b3670f46fe2a2f08187f171817b606dc
robbyxp1 <robxp@hotmail.com>
2018-01-26 13:45:20 +0000

    To 9.0.4

--- EDDiscovery/Properties/AssemblyInfo.cs
+++ EDDiscovery/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.0.3.0")]
-[assembly: AssemblyFileVersion("9.0.3.0")]
+[assembly: AssemblyVersion("9.0.4.0")]
+[assembly: AssemblyFileVersion("9.0.4.0")]
 
