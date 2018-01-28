7100eb34078873b25101661fbd47bb9db2e84de4
robbyxp1 <robxp@hotmail.com>
2018-01-28 17:44:53 +0000

    Added FighterDestroyed/Rebuilt/LaunchDrone/NpcCrePaidwage/NpcCrewRank
    Also, UI events get the whole Flag state, in case there are some
    dependencies between one flag and the rest.
    Test harness can generate above

--- EliteDangerous/EliteDangerous/UIEvents.cs
+++ EliteDangerous/EliteDangerous/UIEvents.cs
@@ -60,30 +60,68 @@ namespace EliteDangerousCore
         InSRV ,
     }
 
+    public enum StatusFlags
+    {
+        Docked = 0, // (on a landing pad)
+        Landed = 1, // (on planet surface)
+        LandingGear = 2,
+        ShieldsUp = 3,
+        Supercruise = 4,
+        FlightAssist = 5,
+        HardpointsDeployed = 6,
+        InWing = 7,
+        Lights = 8,
+        CargoScoopDeployed = 9,
+        SilentRunning = 10,
+        ScoopingFuel = 11,
+        SrvHandbrake = 12,
+        SrvTurret = 13,
+        SrvUnderShip = 14,
+        SrvDriveAssist = 15,
+        FsdMassLocked = 16,
+        FsdCharging = 17,
+        FsdCooldown = 18,
+        LowFuel = 19,
+        OverHeating = 20,
+        HasLatLong = 21,
+        IsInDanger = 22,
+        BeingInterdicted = 23,
+        InMainShip = 24,
+        InFighter = 25,
+        InSRV = 26,
+    }
+
     public abstract class UIEvent
     {
-        public UIEvent(UITypeEnum t, DateTime time)
+        public UIEvent(UITypeEnum t, DateTime time, long totalflags = 0)
         {
             EventTypeID = t;
             EventTimeUTC = time;
+            FlagState = totalflags;
         }
 
-        public DateTime EventTimeUTC;
-        public UITypeEnum EventTypeID;             // name of event. 
+        public DateTime EventTimeUTC { get; set; }
+        public UITypeEnum EventTypeID { get; set; }             // name of event. 
         public string EventTypeStr { get { return EventTypeID.ToString(); } }
+        public long FlagState { get; set; }                   // Flag state total at this event time, if you want to use it
 
         static string JournalRootClassname = typeof(UIEvents.UIDocked).Namespace;        // pick one at random to find out root classname
 
         // Flag Factory (others are created individually)
 
-        static public UIEvent CreateFlagEvent(string name, bool value, DateTime time)
+        static public UIEvent CreateFlagEvent(string name, bool value, DateTime time, long totalflags)
         {
             string evname = "UI" + name;
             Type t = Type.GetType(JournalRootClassname + "." + evname, false, true);
             if (t != null)
-                return (UIEvent)Activator.CreateInstance(t, new Object[] { value, time });
+            {
+                UIEvent e = (UIEvent)Activator.CreateInstance(t, new Object[] { value, time });
+                e.FlagState = totalflags;
+                return e;
+            }
             else
                 System.Diagnostics.Debug.Assert(true);
+
             return null;
         }
     }
