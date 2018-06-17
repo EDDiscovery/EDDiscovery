974726c2cbaf771e7e2ccad9a5f2033531fe6f4d
Ben Peddell <klightspeed@killerwolves.net>
2018-06-18 01:53:00 +1000

    Fix errors in Community Goal submission to Inara

--- EliteDangerous/Inara/Inara.cs
+++ EliteDangerous/Inara/Inara.cs
@@ -663,9 +663,12 @@ namespace EliteDangerousCore.Inara
                 JObject eventData = new JObject();
 
                 eventData["communitygoalGameID"] = goals.CGID;
-                eventData["contribution"] = goals.NumContributors;
+                eventData["contribution"] = goals.PlayerContribution;
                 eventData["percentileBand"] = goals.PlayerPercentileBand;
                 eventData["percentileBandReward"] = goals.Bonus.Value;
+
+                if (goals.PlayerInTopRank == true)
+                    eventData["isTopRank"] = true;
 
                 return Event("setCommanderCommunityGoalProgress", dt, eventData);
             }
