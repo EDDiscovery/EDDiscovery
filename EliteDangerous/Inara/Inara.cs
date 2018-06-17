4f59f969a3b308f12378f782bd57772929d472a3
Ben Peddell <klightspeed@killerwolves.net>
2018-06-18 02:03:22 +1000

    Populate tierMax in setCommunityGoal

--- EliteDangerous/Inara/Inara.cs
+++ EliteDangerous/Inara/Inara.cs
@@ -644,7 +644,8 @@ namespace EliteDangerousCore.Inara
             eventData["goalExpiry"] = goals.Expiry.ToStringZulu();
             if (goals.TierReachedInt.HasValue)
                 eventData["tierReached"] = goals.TierReachedInt.Value;
-            // no evidence of this being in journals eventData["tierMax"] = goals.
+            if (goals.TopTierInt.HasValue)
+                eventData["tierMax"] = goals.TopTierInt;
             if (goals.TopRankSize.HasValue)
                 eventData["topRankSize"] = goals.TopRankSize.Value;
             eventData["isCompleted"] = goals.IsComplete;
