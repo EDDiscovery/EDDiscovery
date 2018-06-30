9a8d71dcc4c2adaae8a731e0b2762f2b0780bc4d
robbyxp1 <robxp@hotmail.com>
2018-06-30 03:35:04 -0400

    Apply surface bookmark/compass fix to master

--- EDDiscovery/UserControls/Helpers/SurfaceBookmarksForm.cs
+++ EDDiscovery/UserControls/Helpers/SurfaceBookmarksForm.cs
@@ -65,7 +65,7 @@ namespace EDDiscovery.UserControls
         public void Init(string systemName, PlanetMarks pm)          // from UCB or Bookmark form, Init with a bookmark
         {
             Edited = false;
-            planetmarks = pm!=null ? pm : new PlanetMarks();
+            planetmarks = pm != null ? pm : new PlanetMarks();
 
             dataGridViewMarks.CancelEdit();
             dataGridViewMarks.ColumnHeadersDefaultCellStyle.BackColor = dataGridViewMarks.RowHeadersDefaultCellStyle.BackColor = EDDTheme.Instance.GridBorderBack;
@@ -83,20 +83,20 @@ namespace EDDiscovery.UserControls
                 {
                     foreach (Location loc in pl.Locations)
                     {
-                        if (((DataGridViewComboBoxCell)dataGridViewMarks.Rows[0].Cells[0]).Items.Contains(pl.Name))
+                        System.Diagnostics.Debug.WriteLine("Rows " + dataGridViewMarks.Rows.Count);
+                        using (DataGridViewRow dr = dataGridViewMarks.Rows[dataGridViewMarks.Rows.Add()])
                         {
-                            using (DataGridViewRow dr = dataGridViewMarks.Rows[dataGridViewMarks.Rows.Add()])
-                            {
-                                dr.Cells[0].Value = pl.Name;
-                                dr.Cells[0].ReadOnly = true;
-                                dr.Cells[1].Value = loc.Name;
-                                dr.Cells[1].ReadOnly = true;
-                                dr.Cells[2].Value = loc.Comment;
-                                dr.Cells[3].Value = loc.Latitude.ToString("F4");
-                                dr.Cells[4].Value = loc.Longitude.ToString("F4");
-                                ((DataGridViewCheckBoxCell)dr.Cells[5]).Value = true;
-                                dr.Tag = loc;
-                            }
+                            if (!BodyName.Items.Contains(pl.Name))      // ensure planet in collection so we don't get errors..
+                                BodyName.Items.Add(pl.Name);
+
+                            dr.Cells[0].Value = pl.Name;
+                            dr.Cells[0].ReadOnly = true;
+                            dr.Cells[1].Value = loc.Name;
+                            dr.Cells[2].Value = loc.Comment;
+                            dr.Cells[3].Value = loc.Latitude.ToString("F4");
+                            dr.Cells[4].Value = loc.Longitude.ToString("F4");
+                            ((DataGridViewCheckBoxCell)dr.Cells[5]).Value = true;
+                            dr.Tag = loc;
                         }
                     }
                 }
@@ -105,12 +105,18 @@ namespace EDDiscovery.UserControls
             sendToCompassToolStripMenuItem.Enabled = true;
 
             dataGridViewMarks.ResumeLayout();
+
+            BaseUtils.Translator.Instance.Translate(this);
+            BaseUtils.Translator.Instance.Translate(contextMenuStrip, this);
         }
 
         public void AddSurfaceLocation(string planet, double latitude, double longitude)    // call After Reset to add a surface bookmark
         {
             using (DataGridViewRow dr = dataGridViewMarks.Rows[dataGridViewMarks.Rows.Add()])
             {
+                if (!BodyName.Items.Contains(planet))
+                    BodyName.Items.Add(planet);
+
                 dr.Cells[0].Value = planet;
                 dr.Cells[0].ReadOnly = true;
                 dr.Cells[1].Value = "Enter a name";
@@ -126,9 +132,7 @@ namespace EDDiscovery.UserControls
         {
             ISystem thisSystem = EDDApplicationContext.EDDMainForm.history.FindSystem(systemName);
 
-            DataGridViewComboBoxColumn c = (DataGridViewComboBoxColumn)dataGridViewMarks.Columns["BodyName"];
-
-            c.Items.Clear();
+            BodyName.Items.Clear();
             if (thisSystem != null)
             {
                 var landables = EDDApplicationContext.EDDMainForm.history.starscan.FindSystem(thisSystem, true)?.Bodies?.Where(b => b.ScanData != null && b.ScanData.IsLandable)?.Select(b => b.fullname);
@@ -138,7 +142,7 @@ namespace EDDiscovery.UserControls
                     foreach (string s in landables)
                     {
                         //System.Diagnostics.Debug.WriteLine("Drop " + s);
-                        c.Items.Add(s);
+                        BodyName.Items.Add(s);
                     }
                 }
             }
@@ -150,7 +154,7 @@ namespace EDDiscovery.UserControls
 
         private void dataGridViewMarks_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
         {
-            if(e.Row.Tag != null)
+            if (e.Row.Tag != null)
             {
                 planetmarks.DeleteLocation(e.Row.Cells[0].Value.ToString(), ((Location)e.Row.Tag).Name);
                 Edited = true;
@@ -173,6 +177,7 @@ namespace EDDiscovery.UserControls
 
         private void dataGridViewMarks_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
+
             DataGridViewRow rw = dataGridViewMarks.Rows[e.RowIndex];
             bool valid = ValidRow(rw);
             ((DataGridViewCheckBoxCell)rw.Cells[5]).Value = valid;
@@ -180,7 +185,7 @@ namespace EDDiscovery.UserControls
             if (valid)
             {
                 Edited = true;
-                //System.Diagnostics.Debug.WriteLine("Commit " + rw.Cells[0].Value.ToString());
+                System.Diagnostics.Debug.WriteLine("Commit " + rw.Cells[0].Value.ToString());
 
                 Location newLoc = rw.Tag != null ? (Location)rw.Tag : new Location();
                 newLoc.Name = rw.Cells[1].Value.ToString();
@@ -189,8 +194,7 @@ namespace EDDiscovery.UserControls
                 newLoc.Longitude = Double.Parse(rw.Cells[4].Value.ToString());
                 planetmarks.AddOrUpdateLocation(rw.Cells[0].Value.ToString(), newLoc);
                 rw.Tag = newLoc;
-                rw.Cells[0].ReadOnly = true;    // can't change the planet or location name once it's in the collection or we'll not be able to look it up again
-                rw.Cells[1].ReadOnly = true;
+                rw.Cells[0].ReadOnly = true;    // can't change the planet.  Can change the name as we are directly editing the loc in memory
                 Changed?.Invoke(planetmarks);
             }
         }
