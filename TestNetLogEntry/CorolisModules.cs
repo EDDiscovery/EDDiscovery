94a9179da1539365894180ab3940d12afe529aa9
robbyxp1 <robxp@hotmail.com>
2018-09-03 14:42:44 +0100

    Fix data to 3.2 standard
    FrontierData made more forgiving and works with what CSV you have made - included a visual basic program to dump all tabs to CSV
    Updated materials, recipes, tech locks, module and ship data
    Corolis data updated and imported

--- TestNetLogEntry/CorolisModules.cs
+++ TestNetLogEntry/CorolisModules.cs
@@ -8,174 +8,8 @@ using System.Threading.Tasks;
 
 namespace NetLogEntry
 {
-    static public class CorolisData
+    static public class CorolisModules
     {
-        static public string ProcessShips(FileInfo[] allFiles)            // overall index of items
-        {
-            string dictofdict = "";
-
-            string returnstring = "";
-
-            returnstring += "      public interface ShipInfo {" + Environment.NewLine +
-                            "      };" + Environment.NewLine;
-
-            returnstring += "      public class ShipModule : ShipInfo {" + Environment.NewLine +
-                            "          public int ModuleID;" + Environment.NewLine +
-                            "          public double Mass;" + Environment.NewLine +
-                            "          public string ModName;" + Environment.NewLine +
-                            "          public string ModType;" + Environment.NewLine +
-                            "          public double Power;" + Environment.NewLine +
-                            "          public string Info;" + Environment.NewLine +
-                            "          public ShipModule(int id, double m, string n , string t) {ModuleID=id;Mass=m;ModName=n;ModType=t;}" + Environment.NewLine +
-                            "          public ShipModule(int id, double m, double p, string i, string n , string t) {ModuleID=id;Mass=m;Power=p;Info=i,ModName=n;ModType=t;}" + Environment.NewLine +
-                            "      };" + Environment.NewLine;
-
-            returnstring += "      public class ShipInfoString : ShipInfo  {" + Environment.NewLine +
-                            "          public string Value;" + Environment.NewLine +
-                            "          public ShipInfoString(string s) {Value=s;}" + Environment.NewLine +
-                            "      };" + Environment.NewLine;
-
-            returnstring += "      public class ShipInfoInt : ShipInfo  {" + Environment.NewLine +
-                            "          public int Value;" + Environment.NewLine +
-                            "          public ShipInfoInt(int i) {Value=i;}" + Environment.NewLine +
-                            "      };" + Environment.NewLine;
-
-            returnstring += "      public class ShipInfoDouble : ShipInfo {" + Environment.NewLine +
-                            "          public double Value;" + Environment.NewLine +
-                            "          public ShipInfoDouble(double d) {Value=d;}" + Environment.NewLine +
-                            "      };" + Environment.NewLine;
-
-            returnstring += "      enum ShipPropID { FDID, HullMass, Name, Manu, Speed, Boost, HullCost, Class };" + Environment.NewLine;
-
-            returnstring += Environment.NewLine;
-
-            string modulestring = "";
-
-            foreach (FileInfo f in allFiles)
-            {
-                string json = File.ReadAllText(f.FullName, Encoding.UTF8);
-
-                JObject jo = new JObject();
-                jo = JObject.Parse(json);
-
-                JToken first = jo.First;        // ship name
-                string name = first.Path;
-                string fdshipname = null;
-
-                foreach (Tuple<string, string> t in SHIP_FD_NAME_TO_CORIOLIS_NAME)
-                {
-                    if (name == t.Item2)
-                    {
-                        fdshipname = t.Item1;
-                        break;
-                    }
-                }
-
-                if ( fdshipname == null)
-                {
-                    Console.WriteLine("Unknown ship " + name);
-                    return "";
-                }
-
-                JObject second = (JObject)first.First;
-
-                JToken bulk = second["bulkheads"];
-
-
-                returnstring += "      static Dictionary<ShipPropID, ShipInfo> " + name + " = new Dictionary<ShipPropID, ShipInfo>" + Environment.NewLine +
-                                     "        {" + Environment.NewLine;
-
-                dictofdict += "            { \"" + fdshipname.ToLowerInvariant() + "\"," + name + "}," + Environment.NewLine;
-
-                JToken prop = second["properties"];
-
-                int index = 0;
-                string[] names = { "Grade1", "Grade2", "Grade3", "Mirrored", "Reactive" };
-                string[] fullnames = { "Lightweight", "Reinforced", "Military", "Mirrored Surface Composite", "Reactive Surface Composite" };
-                foreach (JObject j in bulk.Children())
-                {
-                    int edid = (int)j["edID"];
-                    double mass = (double)j["mass"];
-
-                    string infostr = "";
-                    infostr = infostr.AppendPrePad("Explosive:" + ((double)j["explres"]).ToString("0.#%"), ", ");
-                    infostr = infostr.AppendPrePad("Kinetic:" + ((double)j["kinres"]).ToString("0.#%"), ", ");
-                    infostr = infostr.AppendPrePad("Thermal:" + ((double)j["thermres"]).ToString("0.#%"), ", ");
-
-                    string fdname = fdshipname + "_Armour_" + names[index];
-                    string nicename = (string)prop["name"] + " " + fullnames[index] + " Armour";
-
-                    modulestring += "            { \"" + fdname.ToLowerInvariant() + "\", new ShipModule(" + edid + "," + mass + ",0,\"" + infostr + "\",\""+ nicename + "\",\"Armour\")}," + Environment.NewLine;
-                    index++;
-                }
-
-                returnstring += "            { ShipPropID.FDID, new ShipInfoString(\"" + fdshipname + "\")}," + Environment.NewLine;
-                returnstring += "            { ShipPropID.HullMass, new ShipInfoDouble(" + ((double)prop["hullMass"]) + "F)}," + Environment.NewLine;
-                returnstring += "            { ShipPropID.Name, new ShipInfoString(\"" + ((string)prop["name"]) + "\")}," + Environment.NewLine;
-                returnstring += "            { ShipPropID.Manu, new ShipInfoString(\"" + ((string)prop["manufacturer"]) + "\")}," + Environment.NewLine;
-                returnstring += "            { ShipPropID.Speed, new ShipInfoInt(" + ((string)prop["speed"]) + ")}," + Environment.NewLine;
-                returnstring += "            { ShipPropID.Boost, new ShipInfoInt(" + ((string)prop["boost"]) + ")}," + Environment.NewLine;
-                returnstring += "            { ShipPropID.HullCost, new ShipInfoInt(" + ((string)prop["hullCost"]) + ")}," + Environment.NewLine;
-                returnstring += "            { ShipPropID.Class, new ShipInfoInt(" + ((string)prop["class"]) + ")}," + Environment.NewLine;
-
-                returnstring += "        };" + Environment.NewLine;
-            }
-
-            returnstring += "      static Dictionary<string, Dictionary<ShipPropID,ShipInfo>> ships = new Dictionary<string, Dictionary<ShipPropID,ShipInfo>>" + Environment.NewLine +
-                                     "        {" + Environment.NewLine;
-            returnstring += dictofdict;
-            returnstring += "        };" + Environment.NewLine;
-            returnstring += Environment.NewLine;
-            returnstring += modulestring;
-
-            return returnstring;
-        }
-
-        static List<Tuple<string, string>> SHIP_FD_NAME_TO_CORIOLIS_NAME = new List<Tuple<string, string>>()
-        {
-            // column 2 is the root name node in corolis
-            // column 1 is the upper case name of the FD id
-
-              new Tuple<string,string>("Adder", "adder"),
-              new Tuple<string,string>("Anaconda", "anaconda"),
-              new Tuple<string,string>("Asp", "asp"),
-              new Tuple<string,string>("Asp_Scout", "asp_scout"),
-              new Tuple<string,string>("BelugaLiner", "beluga"),
-              new Tuple<string,string>("CobraMkIII", "cobra_mk_iii"),
-              new Tuple<string,string>("CobraMkIV", "cobra_mk_iv"),
-              new Tuple<string,string>("Cutter", "imperial_cutter"),
-              new Tuple<string,string>("DiamondBackXL", "diamondback_explorer"),
-              new Tuple<string,string>("DiamondBack", "diamondback"),
-              new Tuple<string,string>("Dolphin", "dolphin"),
-              new Tuple<string,string>("Eagle", "eagle"),
-              new Tuple<string,string>("Empire_Courier", "imperial_courier"),
-              new Tuple<string,string>("Empire_Eagle", "imperial_eagle"),
-              new Tuple<string,string>("Empire_Trader", "imperial_clipper"),
-              new Tuple<string,string>("Federation_Corvette", "federal_corvette"),
-              new Tuple<string,string>("Federation_Dropship", "federal_dropship"),
-              new Tuple<string,string>("Federation_Dropship_MkII", "federal_assault_ship"),
-              new Tuple<string,string>("Federation_Gunship", "federal_gunship"),
-              new Tuple<string,string>("FerDeLance", "fer_de_lance"),
-              new Tuple<string,string>("Hauler", "hauler"),
-              new Tuple<string,string>("Krait_MkII", "krait_mkii"),
-              new Tuple<string,string>("Independant_Trader", "keelback"),
-              new Tuple<string,string>("Orca", "orca"),
-              new Tuple<string,string>("Python", "python"),
-              new Tuple<string,string>("SideWinder", "sidewinder"),
-              new Tuple<string,string>("TypeX_3", "alliance_challenger"),
-              new Tuple<string,string>("Type6", "type_6_transporter"),
-              new Tuple<string,string>("Type7", "type_7_transport"),
-              new Tuple<string,string>("Type9", "type_9_heavy"),
-              new Tuple<string,string>("Type9_Military","type_10_defender"),
-              new Tuple<string,string>("TypeX","alliance_chieftain"),
-              new Tuple<string,string>("Viper", "viper"),
-              new Tuple<string,string>("Viper_MkIV", "viper_mk_iv"),
-              new Tuple<string,string>("Vulture", "vulture")
-        };
-
-
-        // New stuff April 2018
-
         static public string ProcessModules(FileInfo[] allFiles)
         {
             string returnstring = "";
@@ -309,29 +143,24 @@ namespace NetLogEntry
 
                 infostr = infostr.Length > 0 ? "\"" + infostr + "\"" : "null";
 
-                if (sym == null)      // missing from corolis data
+                System.Diagnostics.Debug.Assert(sym != null);
+
+                string bettername = GetBetterItemNameEvents(sym);                           // name to give it.. long description text..
+                bettername = betternameprefix + bettername.Replace("  ", " ").Replace("Unk ", "").Trim();
+
+
+                int first_ = sym.IndexOf("_");                                              // modtypename is the type of the module ("Cargo Rack" etc)
+                int second_ = first_ >= 0 ? sym.IndexOf("_", first_ + 1) : -1;              // 
+                int third_ = (second_ >= 0) ? sym.IndexOf("_", second_ + 1) : -1;
+
+                string modtypename = second_ >= 0 ? sym.Substring(0, second_) : sym;        // Modname is normally up to second _ (Int_CargoRack_...)
+
+                if (modtypename == "Int_DroneControl")                     // these need text to third item
                 {
-                    if (edid == 128671347)
-                        sym = "Hpt_MiningLaser_Fixed_Small_Advanced";     // best guess
-                    if (edid == 128671345)
-                        sym = "Hpt_MultiCannon_Fixed_Small_Scatter";
-                    if (edid == 128671342)
-                        sym = "Hpt_PulseLaser_Fixed_Medium_Distruptor";
-                }
-
-                string bettername = GetBetterItemNameEvents(sym);
-                bettername = betternameprefix + bettername.Replace("  ", " ").Trim();
-
-
-                int first_ = sym.IndexOf("_");
-                int second_ = first_ >= 0 ? sym.IndexOf("_", first_ + 1) : -1;
-
-                string modtypename = second_ >= 0 ? sym.Substring(0, second_) : sym;
-
-                if (modtypename == "Int_DroneControl")
-                {
-                    int third_ = sym.IndexOf("_", second_ + 1);
-                    modtypename = sym.Substring(0, third_);
+                    if (third_ == -1)
+                        modtypename = sym;
+                    else
+                        modtypename = sym.Substring(0, third_);
                 }
 
                 modtypename = modtypename.SplitCapsWord().Replace("Hpt_", "").Replace("Int_", "").Replace("_", " ");
@@ -348,9 +177,12 @@ namespace NetLogEntry
                 bool powerisnonint = Math.Floor(power) != power;
 
                 names.Add(sym);
-                string part = "            { \"" + sym + "\", new ShipModule(" + edid + ", " + massstr + ", " + powerstr + ", " +
-                                                                         infostr + "," +
-                                                                        "\"" + bettername + "\", \"" + modtypename + "\")}," + Environment.NewLine;
+                string part = "            { \"" + sym + "\", new ShipModule(" + edid + ", " + 
+                                                                                massstr + ", " + 
+                                                                                powerstr + ", " +
+                                                                                infostr + "," +
+                                                                                "\"" + bettername + "\", " +
+                                                                                "\"" + modtypename + "\")}," + Environment.NewLine;
                 returnstring += part;
             }
 
@@ -496,6 +328,7 @@ namespace NetLogEntry
                   new Tuple<string,string>("Drone Control Resource Siphon", "Limpet Controller"),
                   new Tuple<string,string>("Drone Control Fuel Transfer", "Limpet Controller"),
                   new Tuple<string,string>("Drone Control Collection", "Limpet Controller"),
+                  new Tuple<string,string>("Drone Control Unk Vessel Research", "Research Limpet Controller"),
                   new Tuple<string,string>("Drone Control Repair", "Repair Limpet Controller"),
                   new Tuple<string,string>("Buggy Bay", "Planetary Vehicle Hangar"),
                   new Tuple<string,string>("Engine", "Thrusters"),
