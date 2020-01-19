38d26511c5e3adabc9802128da27d3bdee24db93
Philip Woelfel <philip@woelfel.at>
2020-01-19 02:09:44 +0100

    added toggle fullscreen button to website

--- WebSite/EDD/header.js
+++ WebSite/EDD/header.js
@@ -1,6 +1,7 @@
 function write_header()
 {
-    document.write("<header><h1> Elite Dangerous Discovery </h1> <h1> <img src=\"Images/EdLogo600.png\" alt=\"EDD\" width=\"80\"> </h1> </header>");
+    document.write("<header><h1> Elite Dangerous Discovery </h1> <h1> <input type=\"button\" value=\"fullscreen\" id=\"fullscreen_button\" /><img src=\"Images/EdLogo600.png\" alt=\"EDD\" width=\"80\"> </h1> </header>");
+    document.getElementById('fullscreen_button').onclick = toggle_fullscreen
 }
 
 function write_nav()
@@ -39,3 +40,38 @@ function highlight_nav_tab(tab)     // tab 0,1,2 etc.
     lis[tab].classList.add("tabhighlighted");       // adding a class on, instead of just setting the style backcolor, allows hover to still work.
 }
 
+var elem = document.documentElement;
+function toggle_fullscreen(event)
+{
+    console.log(document.fullscreen)
+    if (!document.fullscreen) {
+        open_fullscreen();
+    }
+    else{
+        close_fullscreen();
+    }
+}
+
+function open_fullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+       elem.msRequestFullscreen();
+    }
+}
+
+function close_fullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}
