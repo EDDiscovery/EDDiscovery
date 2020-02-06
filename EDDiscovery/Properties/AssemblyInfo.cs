63507cf5189cea554792261352d61ed00960ec53
robbyxp1 <robxp@hotmail.com>
2020-02-06 16:25:46 +0000

    11.2.2

--- EDDiscovery/Properties/AssemblyInfo.cs
+++ EDDiscovery/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.2.1.0")]
+[assembly: AssemblyVersion("11.2.2.0")]
 
 
