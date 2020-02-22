EDDiscovery/EDDiscovery

commit 36386f0ab3bec849e671c4aac3e7abdcbcfd5c69

Add Tooltip/pop up to material recipes to show synth/eng vs material
Reverse lookup

EDDiscovery/UserControls/CurrentState/UserControlMaterialCommodities.cs
@@ -195,7 +195,17 @@ namespace EDDiscovery.UserControls
                                                 m != null ? m.Price.ToString("0.#") : "-" };
                         }
 
+                        string s = Recipes.UsedInSythesisByFDName(mcd.FDName, Environment.NewLine);
+                        string e = Recipes.UsedInEngineeringByFDName(mcd.FDName, Environment.NewLine);
+                        s = s.AppendPrePad(e, Environment.NewLine);
+                        string b = Recipes.UsedInTechBrokerUnlocksByFDName(mcd.FDName, Environment.NewLine);
+                        s = s.AppendPrePad(b, Environment.NewLine);
+                        string se = Recipes.UsedInSpecialEffectsyFDName(mcd.FDName, Environment.NewLine);
+                        s = s.AppendPrePad(se, Environment.NewLine);
+
                         dataGridViewMC.Rows.Add(rowobj);
+                        dataGridViewMC.Rows[dataGridViewMC.RowCount - 1].Cells[0].ToolTipText = s;
+                        dataGridViewMC.Rows[dataGridViewMC.RowCount - 1].Tag = s;
                     }
                 }
             }
@@ -244,6 +254,19 @@ namespace EDDiscovery.UserControls
             }
         }
 
+        private void dataGridViewMC_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string mats = (string)dataGridViewMC.Rows[e.RowIndex].Tag;
+            if (mats != null)   // sheer paranoia.
+            {
+                mats = mats.Replace(": ", Environment.NewLine + "      ");
+                ExtendedControls.InfoForm info = new ExtendedControls.InfoForm();
+                info.Info(dataGridViewMC.Rows[e.RowIndex].Cells[0].Value as string, FindForm().Icon, mats);
+                info.Size = new Size(800, 600);
+                info.StartPosition = FormStartPosition.CenterParent;
+                info.ShowDialog(FindForm());
+            }
+        }
     }
 
     public class UserControlMaterials : UserControlMaterialCommodities

EDDiscovery/UserControls/CurrentState/UserControlMaterialCommodities.Designer.cs
@@ -96,8 +96,9 @@ namespace EDDiscovery.UserControls
             this.dataGridViewMC.Name = "dataGridViewMC";
             this.dataGridViewMC.RowHeadersVisible = false;
             this.dataGridViewMC.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.dataGridViewMC.Size = new System.Drawing.Size(691, 534);
+            this.dataGridViewMC.Size = new System.Drawing.Size(688, 534);
             this.dataGridViewMC.TabIndex = 1;
+            this.dataGridViewMC.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewMC_CellDoubleClick);
             this.dataGridViewMC.SortCompare += new System.Windows.Forms.DataGridViewSortCompareEventHandler(this.dataGridViewMC_SortCompare);
             // 
             // NameCol
@@ -153,13 +154,13 @@ namespace EDDiscovery.UserControls
             this.vScrollBarCustomMC.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.vScrollBarCustomMC.HideScrollBar = false;
             this.vScrollBarCustomMC.LargeChange = 0;
-            this.vScrollBarCustomMC.Location = new System.Drawing.Point(691, 0);
+            this.vScrollBarCustomMC.Location = new System.Drawing.Point(688, 0);
             this.vScrollBarCustomMC.Maximum = -1;
             this.vScrollBarCustomMC.Minimum = 0;
             this.vScrollBarCustomMC.MouseOverButtonColor = System.Drawing.Color.Green;
             this.vScrollBarCustomMC.MousePressedButtonColor = System.Drawing.Color.Red;
             this.vScrollBarCustomMC.Name = "vScrollBarCustomMC";
-            this.vScrollBarCustomMC.Size = new System.Drawing.Size(13, 534);
+            this.vScrollBarCustomMC.Size = new System.Drawing.Size(16, 534);
             this.vScrollBarCustomMC.SliderColor = System.Drawing.Color.DarkGray;
             this.vScrollBarCustomMC.SmallChange = 1;
             this.vScrollBarCustomMC.TabIndex = 0;
@@ -262,10 +263,13 @@ namespace EDDiscovery.UserControls
             // 
             this.checkBoxClear.AutoSize = true;
             this.checkBoxClear.CheckBoxColor = System.Drawing.Color.Gray;
+            this.checkBoxClear.CheckBoxDisabledScaling = 0.5F;
             this.checkBoxClear.CheckBoxInnerColor = System.Drawing.Color.White;
             this.checkBoxClear.CheckColor = System.Drawing.Color.DarkBlue;
             this.checkBoxClear.ImageButtonDisabledScaling = 0.5F;
+            this.checkBoxClear.ImageIndeterminate = null;
             this.checkBoxClear.ImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.checkBoxClear.ImageUnchecked = null;
             this.checkBoxClear.Location = new System.Drawing.Point(304, 1);
             this.checkBoxClear.Margin = new System.Windows.Forms.Padding(0, 1, 8, 1);
             this.checkBoxClear.MouseOverColor = System.Drawing.Color.CornflowerBlue;
@@ -279,6 +283,9 @@ namespace EDDiscovery.UserControls
             // 
             // toolTip
             // 
+            this.toolTip.AutoPopDelay = 30000;
+            this.toolTip.InitialDelay = 500;
+            this.toolTip.ReshowDelay = 100;
             this.toolTip.ShowAlways = true;
             // 
             // panelTop

EliteDangerous/MaterialsCommodities/MaterialRecipesData.cs
@@ -117,17 +117,38 @@ namespace EliteDangerousCore
             { }
         }
 
-        public static string UsedInSythesisByFDName(string fdname)
+        public static string UsedInSythesisByFDName(string fdname, string join=",")
         {
             MaterialCommodityData mc = MaterialCommodityData.GetByFDName(fdname);
-            return Recipes.UsedInSynthesisByShortName(mc?.Shortname ?? "--");
+            if (SynthesisRecipesByMaterial.ContainsKey(mc))
+                return String.Join(join, SynthesisRecipesByMaterial[mc].Select(x => x.Name + "-" + x.level + ": " + x.IngredientsStringLong));
+            else
+                return "";
         }
 
-        public static string UsedInSynthesisByShortName(string shortname)
+        public static string UsedInEngineeringByFDName(string fdname, string join = ",")
         {
-            MaterialCommodityData mc = MaterialCommodityData.GetByShortName(shortname);
-            if (SynthesisRecipesByMaterial.ContainsKey(mc))
-                return String.Join(",", SynthesisRecipesByMaterial[mc].Select(x => x.Name + "-" + x.level));
+            MaterialCommodityData mc = MaterialCommodityData.GetByFDName(fdname);
+            if (EngineeringRecipesByMaterial.ContainsKey(mc))
+                return String.Join(join, EngineeringRecipesByMaterial[mc].Select(x => x.modulesstring + " "+ x.Name + "-" + x.level + ": " + x.IngredientsStringLong + " @ " + x.engineersstring));
+            else
+                return "";
+        }
+
+        public static string UsedInSpecialEffectsyFDName(string fdname, string join = ",")
+        {
+            MaterialCommodityData mc = MaterialCommodityData.GetByFDName(fdname);
+            if (SpecialEffectsRecipesByMaterial.ContainsKey(mc))
+                return String.Join(join, SpecialEffectsRecipesByMaterial[mc].Select(x => x.Name + ": " + x.IngredientsStringLong));
+            else
+                return "";
+        }
+
+        public static string UsedInTechBrokerUnlocksByFDName(string fdname, string join = ",")
+        {
+            MaterialCommodityData mc = MaterialCommodityData.GetByFDName(fdname);
+            if (TechBrokerUnlockRecipesByMaterial.ContainsKey(mc))
+                return String.Join(join, TechBrokerUnlockRecipesByMaterial[mc].Select(x => x.Name + ": " + x.IngredientsStringLong));
             else
                 return "";
         }
@@ -1098,8 +1119,13 @@ namespace EliteDangerousCore
             new TechBrokerUnlockRecipe("Plasma Shock Cannon Turret Large","Human","26V,28W,22Re,24Tc,10IOD"),
             new TechBrokerUnlockRecipe("Plasma Shock Cannon Turret Medium","Human","24V,22W,20Re,28Tc,8PTB"),
             new TechBrokerUnlockRecipe("Plasma Shock Cannon Turret Small","Human","8V,12W,10Re,10Tc,4IOD"),
-
         };
+
+        public static Dictionary<MaterialCommodityData, List<TechBrokerUnlockRecipe>> TechBrokerUnlockRecipesByMaterial =
+            TechBrokerUnlocks.SelectMany(r => r.Ingredients.Select(i => new { mat = i, recipe = r }))
+                              .GroupBy(a => a.mat)
+                              .ToDictionary(g => g.Key, g => g.Select(a => a.recipe).ToList());
+
 
         public static List<SpecialEffectRecipe> SpecialEffects = new List<SpecialEffectRecipe>()
         {
@@ -1189,6 +1215,11 @@ namespace EliteDangerousCore
             new SpecialEffectRecipe("Thermo Block", "Shield Generator", "5WSE,3FFC,1HV"),
             new SpecialEffectRecipe("Thermo Block", "Shield Booster", "5ABSD,3CCe,3HV"),
         };
+
+        public static Dictionary<MaterialCommodityData, List<SpecialEffectRecipe>> SpecialEffectsRecipesByMaterial =
+            SpecialEffects.SelectMany(r => r.Ingredients.Select(i => new { mat = i, recipe = r }))
+                              .GroupBy(a => a.mat)
+                              .ToDictionary(g => g.Key, g => g.Select(a => a.recipe).ToList());
 
         #endregion
     }

commit e2e1ec7eea9e6f208c41b3d5e6fd26705cf44c5f

Fix IndexOutOfRangeException in 36386f0
Fixes: 36386f0ab3bec849e671c4aac3e7abdcbcfd5c69 Add Tooltip/pop up to material recipes to show synth/eng vs material

EDDiscovery/UserControls/CurrentState/UserControlMaterialCommodities.cs
@@ -256,15 +256,18 @@ namespace EDDiscovery.UserControls
 
         private void dataGridViewMC_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            string mats = (string)dataGridViewMC.Rows[e.RowIndex].Tag;
-            if (mats != null)   // sheer paranoia.
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridViewMC.Rows.Count)
             {
-                mats = mats.Replace(": ", Environment.NewLine + "      ");
-                ExtendedControls.InfoForm info = new ExtendedControls.InfoForm();
-                info.Info(dataGridViewMC.Rows[e.RowIndex].Cells[0].Value as string, FindForm().Icon, mats);
-                info.Size = new Size(800, 600);
-                info.StartPosition = FormStartPosition.CenterParent;
-                info.ShowDialog(FindForm());
+                string mats = (string)dataGridViewMC.Rows[e.RowIndex].Tag;
+                if (mats != null)   // sheer paranoia.
+                {
+                    mats = mats.Replace(": ", Environment.NewLine + "      ");
+                    ExtendedControls.InfoForm info = new ExtendedControls.InfoForm();
+                    info.Info(dataGridViewMC.Rows[e.RowIndex].Cells[0].Value as string, FindForm().Icon, mats);
+                    info.Size = new Size(800, 600);
+                    info.StartPosition = FormStartPosition.CenterParent;
+                    info.ShowDialog(FindForm());
+                }
             }
         }
     }