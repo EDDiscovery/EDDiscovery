c7af3d80243bbe1e65e26b4082f4964671fa5032
Ben Peddell <klightspeed@killerwolves.net>
2020-03-07 08:35:27 +1000

    Only send codex entries with non-zero voucher amounts

--- EliteDangerous/IGAU/IGAUSync.cs
+++ EliteDangerous/IGAU/IGAUSync.cs
@@ -83,24 +83,27 @@ namespace EliteDangerousCore.IGAU
                         hlscanevent.Reset();
                         JournalCodexEntry c = he.journalEntry as JournalCodexEntry;
 
-                        var msg = igau.CreateIGAUMessage(he.EventTimeUTC.ToStringZulu(),
-                                                              c.EntryID, c.Name, c.Name_Localised, c.System, c.SystemAddress ?? 0);
+                        if (c.VoucherAmount != null && c.VoucherAmount > 0)
+                        {
 
-                        System.Diagnostics.Debug.WriteLine("IGAU Post " + msg.ToString(Newtonsoft.Json.Formatting.Indented));
+                            var msg = igau.CreateIGAUMessage(he.EventTimeUTC.ToStringZulu(),
+                                                                  c.EntryID, c.Name, c.Name_Localised, c.System, c.SystemAddress ?? 0);
 
-                        // comment ball the ack in when your ready to try!
+                            System.Diagnostics.Debug.WriteLine("IGAU Post " + msg.ToString(Newtonsoft.Json.Formatting.Indented));
 
-                        //igau.PostMessage(msg, out bool accepted);
-                        //if (!accepted)
-                        //  logger?.Invoke("IGAU Message rejected " + he.EventTimeUTC.ToStringZulu());
+                            // comment ball the ack in when your ready to try!
+
+                            igau.PostMessage(msg, out bool accepted);
+                            if (!accepted)
+                                logger?.Invoke("IGAU Message rejected " + he.EventTimeUTC.ToStringZulu());
+                        }
 
                         eventcount++;
                     }
                     catch (Exception ex)
                     {
-                        System.Diagnostics.Trace.WriteLine("Exception ex:" + ex.Message);
-                        System.Diagnostics.Trace.WriteLine("Exception ex:" + ex.StackTrace);
-                        logger?.Invoke("EDDN sync Exception " + ex.Message + Environment.NewLine + ex.StackTrace);
+                        System.Diagnostics.Trace.WriteLine("Error sending event to IGAU:" + ex.Message + Environment.NewLine + ex.StackTrace);
+                        logger?.Invoke("IGAU sync Exception " + ex.Message + Environment.NewLine + ex.StackTrace);
                     }
 
                     if (Exit)
@@ -115,7 +118,7 @@ namespace EliteDangerousCore.IGAU
                 SentEvents?.Invoke(eventcount);     // tell the system..
 
                 if (hlscanunsyncedlist.IsEmpty)     // if nothing there..
-                    hlscanevent.WaitOne(60000);     // Wait up to 60 seconds for another EDDN event to come in
+                    hlscanevent.WaitOne(60000);     // Wait up to 60 seconds for another IGAU event to come in
 
                 if (Exit)
                 {
