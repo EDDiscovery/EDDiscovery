e0ed5dfbceffd927a53989534b4a7ea96eeebee9
robbyxp1 <robxp@hotmail.com>
2020-06-09 13:20:43 +0100

    11.5.0
    Innobuild.bat automates some of the build process so you don't have to do so much manually.

--- EDDiscovery/Properties/AssemblyInfo.cs
+++ EDDiscovery/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.4.5.0")]
+[assembly: AssemblyVersion("11.5.0.0")]
 
 
