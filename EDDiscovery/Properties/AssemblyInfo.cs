353ba05d1f44b0c7e21675c1da22f2874303586b
Ben Peddell <klightspeed@killerwolves.net>
2021-01-05 11:59:26 +1000

    Fix CA1416

--- EDDiscovery/Properties/AssemblyInfo.cs
+++ EDDiscovery/Properties/AssemblyInfo.cs
@@ -29,6 +29,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#if NET5_0
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows7.0")]
+#endif
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
