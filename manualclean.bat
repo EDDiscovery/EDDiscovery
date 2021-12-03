6bdeefc505436a0a267ea9c31104057a883224bf
robbyxp1 <robxp@hotmail.com>
2021-12-03 13:43:00 +0000

    Manualclean fixed for DOS and all files

--- manualclean.bat
+++ manualclean.bat
@@ -1,5 +1,25 @@
 cd EDDiscovery
-delete /rp obj bin -confirm
+del /s /y obj bin
 cd ..\EDDIcons
-delete /rp obj bin -confirm
-cd ..
+del /s /y obj bin
+cd ..\ActionLanguage\ActionLanguage
+del /s /y obj bin
+cd ..\..\ExtendedControls\ExtendedControls
+del /s /y obj bin
+cd ..\..\ExtendedControls\ExtendedForms
+del /s /y obj bin
+cd ..\..\CAPI\CAPI
+del /s /y obj bin
+cd ..\..\BaseUtilities\BaseUtilities
+del /s /y obj bin
+cd ..\..\BaseUtilities\SQLLite
+del /s /y obj bin
+cd ..\..\BaseUtilities\Audio
+del /s /y obj bin
+cd ..\..\BaseUtilities\DirectInput
+del /s /y obj bin
+cd ..\..\EliteDangerousCore\EliteDangerous
+del /s /y obj bin
+cd ..\..
+
+
