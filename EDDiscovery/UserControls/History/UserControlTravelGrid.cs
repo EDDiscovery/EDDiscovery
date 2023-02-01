7a7efe0a781c730f2943051bfd8b0d8bc074b796
robbyxp1 <robxp@hotmail.com>
2023-02-01 15:37:29 +0000

    Implemented FSSDiscoveryScan printing correct system signals apply to
    Changed EDC HistoryList FillInformation interfaces

--- EDDiscovery/UserControls/History/UserControlTravelGrid.cs
+++ EDDiscovery/UserControls/History/UserControlTravelGrid.cs
@@ -446,7 +446,7 @@ namespace EDDiscovery.UserControls
                                + $"mc{he.MaterialCommodity}/w{he.Weapons}/s{he.Suits}/l{he.Loadouts}/e{he.Engineering}\r\n"
                                + $"b{he.journalEntry.IsBeta}/h{ he.journalEntry.IsHorizons}/o{ he.journalEntry.IsOdyssey}\r\n"
                                + $"bkt{he.Status.BookedTaxi} d {he.Status.BookedDropship}\r\n"
-                               + $"jcb{he.Status.CurrentBoost} fsds{he.Status.FSDJumpSequence} jm{he.Status.FSDJumpNextSystemName} {he.Status.FSDJumpNextSystemAddress}\r\n"
+                               + $"jcb{he.Status.CurrentBoost} fsds{he.Status.FSDJumpSequence} jm'{he.Status.FSDJumpNextSystemName??"NoJump"}' ad{he.Status.FSDJumpNextSystemAddress??-1}\r\n"
                                + $"tv{he.isTravelling} dist {he.TravelledDistance} sec {he.TravelledSeconds} jmps {he.TravelledJumps}"
                                ;
 
