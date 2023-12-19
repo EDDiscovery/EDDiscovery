363bb325ff5c12b78f71ca4c453e59402295cbb1
robbyxp1 <robxp@hotmail.com>
2023-12-19 13:48:53 +0000

    Moved revision up to show its now back on master

--- EDDiscovery/Properties/AssemblyInfo.cs
+++ EDDiscovery/Properties/AssemblyInfo.cs
@@ -46,6 +46,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*'
-[assembly: AssemblyVersion("16.2.1.0")]
+[assembly: AssemblyVersion("16.2.9.0")]
 
 
