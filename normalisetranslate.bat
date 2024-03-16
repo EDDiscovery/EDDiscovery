c9748f4fadced33fbff9313c1b02f55ba6272be2
robbyxp1 <robxp@hotmail.com>
2024-03-16 17:09:29 +0000

    Changed to use new normalisetranslate

--- normalisetranslate.bat
+++ normalisetranslate.bat
@@ -1,18 +1,9 @@
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex deutsch-de NS
-call copyback.bat
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex chinese-zh NS
-call copyback.bat
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex francais-fr NS
-call copyback.bat
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex italiano-it NS
-call copyback.bat
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex polski-pl NS
-call copyback.bat
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex portugues-pt-br NS
-call copyback.bat
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex russian-ru NS
-call copyback.bat
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex spanish-es NS
-call copyback.bat
-del copyback.bat
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex deutsch-de
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex chinese-zh
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex francais-fr
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex italiano-it
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex polski-pl
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex portugues-pt-br
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex russian-ru
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex spanish-es
 del report.txt
