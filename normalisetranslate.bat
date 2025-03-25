685f79e99f2ffd7e9592556007792fb44dedd010
robbyxp1 <robxp@hotmail.com>
2025-03-25 17:42:17 +0000

    Run normalisation on example-ex to normalise!

--- normalisetranslate.bat
+++ normalisetranslate.bat
@@ -1,10 +1,12 @@
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex deutsch-de
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex chinese-zh
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex francais-fr
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex italiano-it
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex polski-pl
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex portugues-pt-br
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex russian-ru
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex spanish-es
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex deutsch-de renames.txt
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex chinese-zh renames.txt
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex francais-fr renames.txt
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex italiano-it renames.txt
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex polski-pl    renames.txt
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex portugues-pt-br renames.txt
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex russian-ru      renames.txt
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex spanish-es      renames.txt
+eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex example-ex      renames.txt
+
 del report.txt
 del crowdin*.json
