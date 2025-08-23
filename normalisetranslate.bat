205775e86e7758a558150b0bfad3a1b77febf389
Rob <robxp@hotmail.com>
2025-08-23 16:28:02 +0100

    Normalise translate - add SquadronClass, Panel Colour

--- normalisetranslate.bat
+++ normalisetranslate.bat
@@ -1,12 +1,2 @@
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex deutsch-de renames.txt
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex chinese-zh renames.txt
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex francais-fr renames.txt
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex italiano-it renames.txt
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex polski-pl    renames.txt
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex portugues-pt-br renames.txt
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex russian-ru      renames.txt
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex spanish-es      renames.txt
-eddtest normalisetranslate %cd%\eddiscovery\translations 2 example-ex example-ex      renames.txt
-
+eddtest normalisetranslatemkii c:\code\eddiscovery\eddiscovery\translations 2 example-ex example-ex francais-fr chinese-zh deutsch-de italiano-it polski-pl portugues-pt-br russian-ru spanish-es
 del report.txt
-del crowdin*.json
