36fff08a68e0a493dfad1916d7a00aa9b8acb4b0
Rob <robxp@hotmail.com>
2025-12-18 11:16:19 +0000

    Rename Squadron panel

--- EDDiscovery/TabsPanelsAndPopOuts/Panels.cs
+++ EDDiscovery/TabsPanelsAndPopOuts/Panels.cs
@@ -121,7 +121,7 @@ namespace EDDiscovery
             { new PanelInfo( PanelIDs.Statistics, typeof(UserControlStats), "Statistics", "Stats", "Statistics Information") },
             { new PanelInfo( PanelIDs.SuitsWeapons, typeof(UserControlSuitsWeapons), "Suits & Weapons", "SuitsWeapons", "Suits, Loadouts, Weapons") },
             { new PanelInfo( PanelIDs.Carrier, typeof(UserControlCarrier), "Carrier", "Carrier", "Data about your fleet carrier") },
-            { new PanelInfo( PanelIDs.Squadrons, typeof(UserControlSquadronCarrier), "Squadron Carrier", "SquadronCarrier", "Data about your squadron's carrier") },
+            { new PanelInfo( PanelIDs.Squadrons, typeof(UserControlSquadronCarrier), "Squadron/Carrier", "SquadronCarrier", "Data about your squadron") },
             { new PanelInfo( PanelIDs.Colonisation, typeof(UserControlColonisation), "Colonisation", "Colonisation", "Colonisation statistics") },
 
             { new PanelInfo( "Station Data") },
