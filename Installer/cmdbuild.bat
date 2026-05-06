271d476c0595a140f784b628f2e0bf0e348637fa
Rob <robxp@hotmail.com>
2026-05-06 14:36:07 +0100

    19.1.6
    fix for path for people who don't run from c:

--- Installer/cmdbuild.bat
+++ Installer/cmdbuild.bat
@@ -41,7 +41,7 @@ powershell compress-archive -Path ..\..\EDDiscoveryData\ActionFiles\V1\*.* -Dest
 
 echo.
 echo Build %vno%
-"\Program Files (x86)\Inno Setup 6\iscc.exe" /DMyAppVersion=%vno% innoscript.iss
+"%ProgramFiles(x86)%\Inno Setup 6\iscc.exe" /DMyAppVersion=%vno% innoscript.iss
 copy ..\EDDiscovery\bin\Release\EDDiscovery.Portable.Zip installers\EDDiscovery.Portable.%vno%.zip
 certutil -hashfile installers\EDDiscovery-%vno%.exe SHA256 >installers\checksums.%vno%.txt
 certutil -hashfile installers\EDDiscovery.Portable.%vno%.zip SHA256 >>installers\checksums.%vno%.txt
